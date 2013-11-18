d8ce419b02f07640729ba7d028bb528c0dc636e1
daormar <daormar2@gmail.com>
2013-11-18 11:15:00 +0100

    Added some developer helper files.

---
+++ utils/.copy_thot_pkg_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Automatically copies source files that are necessary to build the
+# package.
+
+this_pkg_name="thot"
+this_pkg_home=${THOT_GITHUB_HOME}
+
+copy_pkg_files.sh ${this_pkg_name} ${this_pkg_home}
---
+++ utils/.gen_thot_pkg_am_vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Automatically generate automake variables for each package.
+
+this_pkg_home=${THOT_GITHUB_HOME}
+
+gen_pkg_am_vars.sh ${this_pkg_home}
