a3007beaf51349b2082c204ae948709708a3d280
daormar <daormar2@gmail.com>
2013-11-26 18:11:09 +0100

    Little modification in helper script.

--- utils/.git_rm_thot_pkg_files.sh
+++ utils/.git_rm_thot_pkg_files.sh
@@ -4,8 +4,13 @@
 this_pkg_home=${THOT_GITHUB_HOME}
 
 # Remove files in utils dir
+# .sh
 git rm `find ${this_pkg_home}/utils -name "*.sh" | grep -v "utils/\."`
-git rm `find ${this_pkg_home}/utils -name "*.pl"`
+
+# .pl
+for file in `find ${this_pkg_home}/utils -name "*.pl"`; do
+    git rm $file
+done
 
 # Remove files in src dir
 git rm `find ${this_pkg_home}/src -name "*.h" | grep -v "thot_config.h"`
