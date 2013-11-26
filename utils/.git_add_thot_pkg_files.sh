d833c32c41a204490ff6f1cedf54f1a390cacd94
daormar <daormar2@gmail.com>
2013-11-26 18:10:20 +0100

    Little modification in helper script.

--- utils/.git_add_thot_pkg_files.sh
+++ utils/.git_add_thot_pkg_files.sh
@@ -4,8 +4,13 @@
 this_pkg_home=${THOT_GITHUB_HOME}
 
 # Add files in utils dir
+# .sh
 git add `find ${this_pkg_home}/utils -name "*.sh"| grep -v "utils/\."`
-git add `find ${this_pkg_home}/utils -name "*.pl"`
+
+# .pl
+for file in `find ${this_pkg_home}/utils -name "*.pl"`; do
+    git add $file
+done
 
 # Add files in src dir
 git add `find ${this_pkg_home}/src -name "*.h" | grep -v "thot_config.h"`
