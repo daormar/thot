d27a3f9d5e57cb199485111d07c2d885f7b5c77a
daormar <daormar2@gmail.com>
2013-12-05 15:37:17 +0100

    Solved some portability problems with the qsub command.

--- utils/incr_models/thot_pbs_get_ngram_counts.sh
+++ utils/incr_models/thot_pbs_get_ngram_counts.sh
@@ -43,6 +43,9 @@ usage()
     echo "                     NOTES:"
     echo "                      a) give absolute paths when using pbs clusters."
     echo "                      b) ensure there is enough disk space in the partition."
+    echo "--sync-sleep       : Use sleep command loops to synchronize process (only for"
+    echo "                     pbs clusters). This is useful when the number of jobs that"
+    echo "                     can be executed with qsub is restricted."
     echo "-debug             : After ending, do not delete temporary files"
     echo "                     (for debugging purposes)."
     echo "--help             : Display this help and exit."
@@ -92,9 +95,11 @@ set_shared_dir()
     chunks_dir=$SDIR/chunks
     scripts_dir=$SDIR/scripts
     counts_per_chunk_dir=$SDIR/counts_per_chunk
+    sync_info_dir=$SDIR/sync
     mkdir ${chunks_dir} || return 1
     mkdir ${scripts_dir} || return 1
     mkdir ${counts_per_chunk_dir} || return 1
+    mkdir ${sync_info_dir} || return 1
 
     # Function executed correctly
     return 0
@@ -215,42 +220,106 @@ launch()
     local suffix=$3
     local outvar=$4
 
-    if [ -z "$QSUB" ]; then
+    if [ -z "${QSUB}" ]; then
         $program &
         eval "${outvar}=$!"
     else
-        # Synchronization is carried out by explicitly defining
-        # dependencies between jobs when executing qsub
+        # Check if the sleep command is used to synchronize processes
+        if [ ${sync_sleep} -eq 1 ]; then
+            # The sleep command is being used
+            # Create script
+            create_script ${scripts_dir}/${program}${suffix}.sh $program
+            # Execute qsub command
+            local jid=$(${QSUB} ${QSUB_TERSE_OPT} ${qs_opts} ${scripts_dir}/${program}${suffix}.sh | ${TAIL} -1)
+
+            # Set value of output variable
+            eval "${outvar}='${jid}'"
+        else
+            # Synchronization is carried out by explicitly defining
+            # dependencies between jobs when executing qsub
             
-        # Create script
-        create_script ${scripts_dir}/${program}${suffix}.sh $program
+            # Create script
+            create_script ${scripts_dir}/${program}${suffix}.sh $program
 
-        # Define qsub option declaring job dependencies
-        local depend_opt=""
-        if [ ! -z "$job_deps" ]; then
-            job_deps=`echo ${job_deps} | $AWK '{for(i=1;i<NF;++i) printf"%s:",$i; printf"%s",$NF}'`
-            depend_opt="-W depend=afterok:${job_deps}"
+            # Define qsub option declaring job dependencies
+            local depend_opt=""
+            if [ ! -z "$job_deps" ]; then
+                job_deps=`echo ${job_deps} | $AWK '{for(i=1;i<NF;++i) printf"%s:",$i; printf"%s",$NF}'`
+                depend_opt="-W depend=afterok:${job_deps}"
+            fi
+            
+            # Execute qsub command. The -h option is used to hold
+            # jobs. All jobs are released at the end of the script. This
+            # ensures that job dependencies are defined over existing
+            # jobs
+            local jid=$(${QSUB} ${QSUB_TERSE_OPT} -h ${depend_opt} ${qs_opts} ${scripts_dir}/${program}${suffix}.sh | ${TAIL} -1)
+
+            # Set value of output variable
+            eval "${outvar}='${jid}'"
+
+            # Uncomment line to show debug information
+            # echo $program ${depend_opt} $jid
         fi
+    fi
+}
 
-        # Execute qsub command. The -h option is used to hold
-        # jobs. All jobs are released at the end of the script. This
-        # ensures that job dependencies are defined over existing
-        # jobs
-        local jid=$(${QSUB} -h ${depend_opt} ${qs_opts} ${scripts_dir}/${program}${suffix}.sh)
+pbs_sync()
+{
+    # Init vars
+    local job_ids=$1
+    local pref=$2
+    local sync_num_files=`echo "${job_ids}" | $AWK '{printf"%d",NF}'`
 
-        # Set value of output variable
-        eval "${outvar}='${jid}'"
-
-        # Uncomment line to show debug information
-        # echo $program ${depend_opt} $jid
+    if [ ${sync_sleep} -eq 1 ]; then
+        # Execute sync loop
+        local sync_end=0
+        while [ ${sync_end} -ne 1 ]; do
+            sleep 2
+            
+            # Compare current number of sync files written with the required
+            # number
+            sync_curr_num_files=`ls -l ${sync_info_dir}/ | grep " ${pref}" | wc -l`
+            if [ ${sync_curr_num_files} -eq ${sync_num_files} ]; then
+                sync_end=1
+            fi
+      
+            # Sanity check
+            # In pbs clusters, check if there are terminated processes
+            # that have not written the sync file
+            num_running_procs=0
+            for jid in ${job_ids}; do
+                nl=`$QSTAT ${jid} 2>&1 | grep "Unknown" | wc -l`
+                if [ $nl -eq 0 ]; then
+                    num_running_procs=`expr ${num_running_procs} + 1`
+                fi
+            done
+            if [ ${num_running_procs} -eq 0 ]; then
+                sync_curr_num_files=`ls -l ${sync_info_dir}/ | grep " ${pref}" | wc -l`
+                if [ ${sync_curr_num_files} -ne ${sync_num_files} ]; then
+                    echo "Error during synchronization"
+                    return 1
+                fi
+            fi
+        done
+      
+        return 0
+    else
+        # No sync loop is required
+        return 0
     fi
 }
 
 sync()
 {
-    if [ -z "$QSUB" ]; then
+    # Init vars
+    local job_ids=$1
+    local pref=$2
+
+    if [ -z "${QSUB}" ]; then
         wait
         return 0
+    else
+        pbs_sync "${job_ids}" $pref
     fi
 }
 
@@ -304,6 +373,9 @@ proc_chunk()
     # Write date to log file
     echo "Processing of chunk ${chunk} finished ("`date`")" >> $SDIR/log 
 
+    # Create sync file
+    echo "" > ${sync_info_dir}/proc_chunk_${chunk}
+
     return 0
 }
 
@@ -334,6 +406,9 @@ generate_counts_file()
     # Copy log file
     echo "**** Parallel process finished at: "`date` >> $SDIR/log
     cp $SDIR/log ${output}.log
+
+    # Create sync file
+    echo "" > ${sync_info_dir}/generate_counts_file
 }
 
 pr_given=0
@@ -344,6 +419,7 @@ c_given=0
 n_given=0
 o_given=0
 tdir="/tmp"
+sync_sleep=0
 debug=0
 
 if [ $# -eq 0 ]; then
@@ -414,6 +490,8 @@ while [ $# -ne 0 ]; do
         "-unk") unk_given=1
             ;;
         "-debug") debug=1
+            ;;
+        "--sync-sleep") sync_sleep=1
             ;;
     esac
     shift
@@ -493,18 +571,24 @@ job_deps=${pc_job_ids}
 launch "${job_deps}" generate_counts_file "" gcf_job_id || exit 1
 
 # Generate counts file synchronization
-sync || exit 1
+sync "${gcf_job_id}" "generate_counts_file" || exit 1
 
 # Remove temporary files
-job_deps=${gcf_job_id}
-launch "${job_deps}" remove_temp "" rt_job_id || exit 1
-# Remove temporary files synchronization
-sync || exit 1
+if [ ${sync_sleep} -eq 1 ]; then
+    # Sync using sleep is enabled
+    remove_temp
+else
+    # Sync using sleep is not enabled
+    job_deps=${gcf_job_id}
+    launch "${job_deps}" remove_temp "" rt_job_id || exit 1
+    # Remove temporary files synchronization
+    sync "${rt_job_id}" "remove_temp" || exit 1
+fi
 
 # Update job_id_list
 job_id_list="${pc_job_ids} ${gcf_job_id} ${rt_job_id}"
 
 # Release job holds
-if [ ! -z "${QSUB}" ]; then
+if [ ! -z "${QSUB}" -a ${sync_sleep} -eq 0 ]; then
     release_job_holds "${job_id_list}"
 fi
