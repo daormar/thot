545d86bb50b0315d16d665b786c3831a7d88ffe3
daormar <daormar2@gmail.com>
2015-08-05 11:51:43 +0200

    Correct help message.

--- utils/phrase_models/thot_pbs_filter_ttable.sh
+++ utils/phrase_models/thot_pbs_filter_ttable.sh
@@ -117,14 +117,13 @@ if [ $# -eq 0 ]; then
     echo "                              [-sdir <string>] [-T <string>] [-debug]" >&2
     echo "" >&2
     echo "-t <string>         : Thot translation table" >&2
-    echo "                      (can be read from stdin)." >&2
-    echo "-c <string>         : Test corpus to be translated." >&2
-    echo "-o <string>         : Output file." >&2
+    echo "-c <string>         : Test corpus to be translated" >&2
+    echo "-o <string>         : Output file" >&2
     echo "-n <int>            : Maximum number of translation options for each target" >&2
     echo "                      phrase that are considered during a translation process" >&2
-    echo "                      ("${N_DEFAULT}" by default)." >&2
+    echo "                      ("${N_DEFAULT}" by default)" >&2
     echo "-qs <string>        : Specific options to be given to the qsub command"
-    echo "                      (example: -qs \"-l pmem=1gb\")."
+    echo "                      (example: -qs \"-l pmem=1gb\")"
     echo "-sdir <string>      : Absolute path of a directory common to all" >&2
     echo "                      processors. If not given, \$HOME will be used" >&2
     echo "-T <string>         : Use <string> for temporaries instead of /tmp" >&2
@@ -194,7 +193,7 @@ else
     # verify parameters
 
     if [ ${t_given} -eq 0 ]; then
-        echo "Error: -t option not given"
+        echo "Error: -t option not given" >&2
         exit 1        
     else
         if [ ! -f ${ttable_file} ]; then
