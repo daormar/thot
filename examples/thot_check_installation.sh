e1a62e85384a0b3235eeaa503b3768c1910e30e9
daormar <daormar2@gmail.com>
2015-09-17 14:14:57 +0200

    Correct problem with shared dir on pbs clusters.

--- examples/thot_check_installation.sh
+++ examples/thot_check_installation.sh
@@ -14,14 +14,50 @@ echo ""
 debugdir=$tmpdir/debug
 mkdir $debugdir
 
+# Create directory for shared information
+sdir=`mktemp -d $HOME/thot_installcheck_XXXXXX`
+
+# Print warning about possible resource requirements when the software
+# is installed on computer clusters
+if [ ${QSUB_WORKS} = "yes" ]; then
+
+    qs_opt="-qs"
+
+    echo "------------------------------------------------------------------------" >&2
+    echo "NOTE: package was installed on a PBS cluster, you may need to reserve" >&2
+    echo "cluster resources in order to successfully execute the Thot tools." >&2
+    echo "" >&2
+    echo "To reserve resources for this checking, export the \"qs_par\" variable." >&2
+    if [ -z "${qs_par}" ]; then
+        echo "Warning: the \"qs_par\" variable is empty." >&2
+        echo "Sample initialisations for the \"qs_par\":" >&2
+        echo "" >&2
+        echo "export qs_par=\"-l pmem=1gb\"" >&2
+        echo "export qs_par=\"-l h_vmem=4G,h_rt=10:00:00\"" >&2
+    else
+        echo "Current value of \"qs_par\": \"${qs_par}\"" >&2
+    fi
+    echo "" >&2
+    echo "Please consider the use of the -qs option with the different tools" >&2
+    echo "provided by Thot." >&2
+    echo "------------------------------------------------------------------------" >&2
+    echo "" >&2
+    sleep 5
+
+else
+    qs_opt=""
+    qs_par=""
+fi
+
 # Check thot_lm_train
 echo "**** Checking thot_lm_train..."
 echo ""
 ${bindir}/thot_lm_train -c $datadir/toy_corpus/en.train -o $tmpdir/lm -n 4 -unk \
-     -tdir $debugdir -sdir $debugdir -debug
+     -tdir $debugdir -sdir $sdir ${qs_opt} "${qs_par}" -debug
 if test $? -eq 0 ; then
     echo "... Done"
 else
+    mv $sdir/* $debugdir
     echo "================================================"
     echo " Test failed!"
     echo " See additional information in ${tmpdir}"
@@ -36,10 +72,11 @@ echo ""
 echo "**** Checking thot_tm_train..."
 echo ""
 ${bindir}/thot_tm_train -s $datadir/toy_corpus/sp.train -t $datadir/toy_corpus/en.train \
-    -o $tmpdir/tm -n 5 -tdir $debugdir -sdir $debugdir -debug
+    -o $tmpdir/tm -n 5 -tdir $debugdir -sdir $sdir ${qs_opt} "${qs_par}" -debug
 if test $? -eq 0 ; then
     echo "... Done"
 else
+    mv $sdir/* $debugdir
     echo "================================================"
     echo " Test failed!"
     echo " See additional information in ${tmpdir}"
@@ -57,6 +94,7 @@ ${bindir}/thot_gen_cfg_file $tmpdir/lm/lm_desc $tmpdir/tm/tm_desc > $tmpdir/serv
 if test $? -eq 0 ; then
     echo "... Done"
 else
+    mv $sdir/* $debugdir
     echo "================================================"
     echo " Test failed!"
     echo " See additional information in ${tmpdir}"
@@ -71,10 +109,11 @@ echo ""
 echo "**** Checking thot_smt_tune..."
 echo ""
 ${bindir}/thot_smt_tune -c $tmpdir/server.cfg -s $datadir/toy_corpus/sp.dev -t $datadir/toy_corpus/en.dev \
-    -o $tmpdir/tune  -tdir $debugdir -sdir $debugdir -debug
+    -o $tmpdir/tune  -tdir $debugdir -sdir $sdir ${qs_opt} "${qs_par}" -debug
 if test $? -eq 0 ; then
     echo "... Done"
 else
+    mv $sdir/* $debugdir
     echo "================================================"
     echo " Test failed!"
     echo " See additional information in ${tmpdir}"
@@ -88,10 +127,12 @@ echo ""
 # Check thot_prepare_sys_for_test
 echo "**** Checking thot_prepare_sys_for_test..."
 echo ""
-${bindir}/thot_prepare_sys_for_test -c $tmpdir/tune/tuned_for_dev.cfg -t $datadir/toy_corpus/sp.test -o $tmpdir/systest
+${bindir}/thot_prepare_sys_for_test -c $tmpdir/tune/tuned_for_dev.cfg -t $datadir/toy_corpus/sp.test \
+    ${qs_opt} "${qs_par}" -o $tmpdir/systest -tdir $debugdir -sdir $sdir
 if test $? -eq 0 ; then
     echo "... Done"
 else
+    mv $sdir/* $debugdir
     echo "================================================"
     echo " Test failed!"
     echo " See additional information in ${tmpdir}"
@@ -106,10 +147,11 @@ echo ""
 echo "**** Checking thot_decoder..."
 echo ""
 ${bindir}/thot_decoder -c $tmpdir/systest/test_specific.cfg -t $datadir/toy_corpus/sp.test \
-    -o $tmpdir/thot_decoder_out -sdir $debugdir -debug
+    -o $tmpdir/thot_decoder_out -sdir $debugdir ${qs_opt} "${qs_par}" -debug
 if test $? -eq 0 ; then
     echo "... Done"
 else
+    mv $sdir/* $debugdir
     echo "================================================"
     echo " Test failed!"
     echo " See additional information in ${tmpdir}"
@@ -127,6 +169,7 @@ ${bindir}/thot_calc_bleu -r $datadir/toy_corpus/en.test -t $tmpdir/thot_decoder_
 if test $? -eq 0 ; then
     echo "... Done"
 else
+    mv $sdir/* $debugdir
     echo "================================================"
     echo " Test failed!"
     echo " See additional information in ${tmpdir}"
@@ -137,10 +180,11 @@ fi
 
 echo ""
 
-# Remove directory for temporaries
-echo "*** Remove directory for temporary files..."
+# Remove directories for temporaries
+echo "*** Remove directories used to store temporary files..."
 echo ""
 rm -rf $tmpdir
+rm -rf $sdir
 
 echo ""
 echo "================"
