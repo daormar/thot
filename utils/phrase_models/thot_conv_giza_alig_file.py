f7740211466cc1f13d8078f46b25cf5e63208aba
Daniel Ortiz <daniel.o@webinterpret.com>
2016-02-23 11:15:33 +0100

    Extend script functionality.

--- utils/phrase_models/thot_conv_giza_alig_file.py
+++ utils/phrase_models/thot_conv_giza_alig_file.py
@@ -25,6 +25,7 @@ def extract_src_info(src_info_array):
     # Initialize variables
     src_words=[]
     aligs=[]
+    wamatrix={}
 
     # Iterate over src_info_array elements
     i=0
@@ -33,12 +34,94 @@ def extract_src_info(src_info_array):
         i+=2
         j=i
         while src_info_array[j]!="})":
-            aligs.append((len(src_words)-1,int(src_info_array[j])))
+            srcpos=len(src_words)-1
+            trgpos=int(src_info_array[j])
+            aligs.append((srcpos,trgpos))
+            wamatrix[str(srcpos)+" "+str(trgpos)]=1
             j+=1
         i=j+1
 
     # Return result
-    return (src_words,aligs)
+    return (src_words,aligs,wamatrix)
+
+##################################################
+def conv_giza_file_to_human(gfile):
+    # read giza file entry by entry
+    while True:
+        # Read three-line entry
+        line1 = gfile.readline()
+        line1=line1.strip("\n")
+
+        line2 = gfile.readline()
+        line2=line2.strip("\n")
+        trgw_array=line2.split()
+
+        line3 = gfile.readline()
+        line3=line3.strip("\n")
+        src_info_array=line3.split()
+
+        if not line3: break 
+
+        # Process entry
+
+        # Extract information
+        (srcw_array,aligs,wamatrix)=extract_src_info(src_info_array)
+
+        # Print entry information
+        ## print header
+        print line1.encode("utf-8")
+        print "trg:",line2.encode("utf-8")	
+        print "src:",
+        for j in range(1,len(srcw_array)):
+            print srcw_array[j].encode("utf-8"),
+        print ""
+	   
+        # ## print matrix
+        # for j in range(1,len(srcw_array)):
+        #     srcpos=len(srcw_array)-j
+        #     for i in range(1,len(trgw_array)+1):
+        #         trgpos=i
+        #         if(str(srcpos)+" "+str(trgpos) in wamatrix.keys()):
+        #             print " 1",
+        #         else:
+        #             print " 0",
+        #     print "|",srcw_array[srcpos].encode("utf-8") #,srcpos,i
+
+        # sep_str=""
+        # for i in range(1,len(trgw_array)+1):
+        #     sep_str=sep_str+"---"
+        # print sep_str
+
+        ## print matrix
+        for i in range(1,len(trgw_array)+1):
+            trgpos=len(trgw_array)-i+1
+            for j in range(1,len(srcw_array)):
+                srcpos=j
+                if(str(srcpos)+" "+str(trgpos) in wamatrix.keys()):
+                    print " 1",
+                else:
+                    print " 0",
+            print "|",trgw_array[trgpos-1].encode("utf-8")
+
+        sep_str=""
+        for j in range(1,len(srcw_array)):
+            sep_str=sep_str+"---"
+        print sep_str
+
+        ## print source words
+        end=False
+        l=0
+        while not end:
+            end=True
+            for j in range(1,len(srcw_array)):
+                srcc_array=list(srcw_array[j])
+                if(l<len(srcc_array)):
+                    print " "+srcc_array[l].encode("utf-8"),
+                    end=False
+                else:
+                    print "  ",
+            print "";
+            l+=1
 
 ##################################################
 def conv_giza_file_to_moses(gfile):
@@ -62,13 +145,12 @@ def conv_giza_file_to_moses(gfile):
         # Process entry
 
         # Extract information
-        (srcw_array,aligs)=extract_src_info(src_info_array)
+        (srcw_array,aligs,wamatrix)=extract_src_info(src_info_array)
 
         # Print entry information
         for i in range(len(aligs)):
             srcpos=aligs[i][0]
             trgpos=aligs[i][1]
-#            print srcw_array[srcpos]+"-"+trgw_array[trgpos-1]+" ",
             if srcpos>0:
                 print str(srcpos-1)+"-"+str(trgpos-1),
 
@@ -123,7 +205,7 @@ def main(argv):
 
     # process parameters
     if(f_val==1):
-        print >> sys.stderr, "Warning, option not implemented"
+        conv_giza_file_to_human(gfile)
     elif(f_val==2):
         conv_giza_file_to_moses(gfile)
     elif(f_val==3):
