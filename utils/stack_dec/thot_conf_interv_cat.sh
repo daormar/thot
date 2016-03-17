e930f76facfd9e1164973902aebf1c7f9f2f8a55
Daniel Ortiz <daniel.o@webinterpret.com>
2016-03-17 16:39:34 +0100

    Refactor code.

--- utils/stack_dec/thot_conf_interv_cat.sh
+++ utils/stack_dec/thot_conf_interv_cat.sh
@@ -6,7 +6,100 @@
 
 # \textbf{Categ}: evaluation acl09
 
-SCR=${0##*/};
+#####################################################################
+standard_eval()
+{
+    $GREP "PARTIAL" $res | \
+        $AWK 'BEGIN{
+                    chars=0
+                    ks=0
+                    ksma=0
+                   }
+                   {
+                    chars+=$6
+                    ks+=$(10)
+                    ksma+=$(17)
+                   }
+                END{
+                    KSR=(ks/chars)*100
+                    KSMR=(ksma/chars)*100
+                    printf"%f\n",KSMR
+                    printf"%f\n",KSR
+                   }'
+}
+
+#####################################################################
+bootstrap_exper()
+{
+    r=$RANDOM
+    $GREP "PARTIAL" $res | \
+        $AWK -v seed=$r -v S=$S -v measure=$measure -v tmp=$TMP '
+             {
+              resh[NR]=$0;
+             }
+          END{
+              chars=0
+              ks=0
+              ksma=0
+              srand(seed);
+              for(i=1;i<=S;i++)
+              {
+               r=rand();
+               id=1+int(NR*r);
+               split(resh[id],fields)
+               chars+=fields[6]
+               ks+=fields[10]
+               ksma+=fields[17]
+              }
+              KSR=(ks/chars)*100
+              KSMR=(ksma/chars)*100
+              if(measure=="KSMR") printf"%f\n",KSMR
+              else printf"%f\n",KSR
+             }'
+}
+
+#####################################################################
+bootstrap_eval()
+{
+    # Perform bootstrap experiments
+    RANDOM=$seed
+    n=1
+    while [ $n -lt $N ]; do
+        bootstrap_exper >> $TMP/scef
+        n=`expr $n + 1`
+    done
+
+    # Obtain mean and stdev
+    mean=`cat $TMP/scef | $AWK '{sum+=$0;}END{printf "%f", sum/NR;}'`
+    echo "Mean: $mean"
+    stDev=`cat $TMP/scef | $AWK -v med=$mean '{aux=$0-med; stDev+=aux*aux;}END{printf "%f", sqrt(stDev/(NR-1));}'`
+    echo "stDev: $stDev"
+    echo;
+
+    # Obtain confidence intervals
+    $SORT -n $TMP/scef > $TMP/scef.sorted;
+    l95=`echo "" | $AWK -v N=$N '{printf"%d",0.025*N+1}' `
+    l90=`echo "" | $AWK -v N=$N '{printf"%d",0.05*N+1}' `;
+    s95=`echo "" | $AWK -v N=$N -v l95=${l95} '{printf"%d",N-l95+1}' `;
+    s90=`echo "" | $AWK -v N=$N -v l90=${l90} '{printf"%d",N-l90+1}' `;
+
+    lb95=`cat $TMP/scef.sorted | head -${l95}  | tail -1`;
+    ub95=`cat $TMP/scef.sorted | head -${s95} | tail -1`;
+    center95=`echo "" | $AWK -v l=$lb95 -v u=$ub95 '{printf"%f",l+(u-l)/2}'`
+    rad95=`echo "" | $AWK -v l=$lb95 -v u=$ub95 '{printf"%f",(u-l)/2}'`
+    lb90=`cat $TMP/scef.sorted | head -$l90  | tail -1`;
+    ub90=`cat $TMP/scef.sorted | head -$s90 | tail -1`;
+    center90=`echo "" | $AWK -v l=$lb90 -v u=$ub90 '{printf"%f",l+(u-l)/2}'`
+    rad90=`echo "" | $AWK -v l=$lb90 -v u=$ub90 '{printf"%f",(u-l)/2}'`
+
+    # Output results
+    echo "Confidence intervals";
+    echo "conf.int.95 = [ $lb95 , $ub95 ] $center95 +- $rad95"
+    echo "conf.int.90 = [ $lb90 , $ub90 ] $center90 +- $rad90"
+    echo ""
+}
+
+#####################################################################
 
 if [ $# -ne 4 -a $# -ne 5 ]; then 
     echo "Computes significance tests"
@@ -21,11 +114,8 @@ if [ $# -ne 4 -a $# -ne 5 ]; then
     exit 1; 
 fi
 
+# Initialize variables
 export LC_ALL=C;
-
-TMP=${TMPDIR:-/tmp}/$SCR.$$
-trap "rm -rf $TMP* 2>/dev/null" EXIT
-mkdir $TMP
 
 seed=$1   # Random seed
 res=$2;   # File with cat results
@@ -44,84 +134,20 @@ if [ ! -f $res ]; then
     exit 1
 fi
 
+# Create directory for temporary files
+TMP=`mktemp -d`
+trap "rm -rf $TMP* 2>/dev/null" EXIT
+
 # Print random seed
 echo "Random seed: $seed"
-echo;
+echo ""
 
-# Standard evaluation
+# Output result of standard evaluation
 echo "Standard evaluation:"
-grep "PARTIAL" $res | \
-    $AWK 'BEGIN{
-     chars=0
-     ks=0
-     ksma=0
-    }
-    {
-     chars+=$6
-     ks+=$(10)
-     ksma+=$(17)
-    }
-    END{
-        KSR=(ks/chars)*100
-        KSMR=(ksma/chars)*100
-        printf"%f\n",KSMR
-        printf"%f\n",KSR
-       }'
+standard_eval
+echo ""
 
-echo;
-
-# Bootstrap Effron93
-RANDOM=$seed
-for ((n=1;n<=N;n++)); do
-    r=$RANDOM
-    grep "PARTIAL" $res | \
-        $AWK -v seed=$r -v S=$S -v measure=$measure -v tmp=$TMP '
-    {resh[NR]=$0;}
-    END{
-        chars=0
-        ks=0
-        ksma=0
-        srand(seed);
-        for(i=1;i<=S;i++)
-        {
-         r=rand();
-         id=1+int(NR*r);
-         split(resh[id],fields)
-         chars+=fields[6]
-         ks+=fields[10]
-         ksma+=fields[17]
-        }
-        KSR=(ks/chars)*100
-        KSMR=(ksma/chars)*100
-        if(measure=="KSMR") printf"%f\n",KSMR
-        else printf"%f\n",KSR
-       }' >> $TMP/scef
-done
-
-# Mean and stDev
-mean=`cat $TMP/scef | $AWK '{sum+=$0;}END{printf "%f", sum/NR;}'`
-echo "Mean: $mean"
-stDev=`cat $TMP/scef | $AWK -v med=$mean '{aux=$0-med; stDev+=aux*aux;}END{printf "%f", sqrt(stDev/(NR-1));}'`
-echo "stDev: $stDev"
-echo;
-
-# Confidence interval
-sort -n $TMP/scef > $TMP/scef.sorted;
-l95=`echo "" | $AWK -v N=$N '{printf"%d",0.025*N+1}' `
-l90=`echo "" | $AWK -v N=$N '{printf"%d",0.05*N+1}' `;
-s95=`echo "" | $AWK -v N=$N -v l95=${l95} '{printf"%d",N-l95+1}' `;
-s90=`echo "" | $AWK -v N=$N -v l90=${l90} '{printf"%d",N-l90+1}' `;
-
-lb95=`cat $TMP/scef.sorted | head -${l95}  | tail -1`;
-ub95=`cat $TMP/scef.sorted | head -${s95} | tail -1`;
-center95=`echo "" | $AWK -v l=$lb95 -v u=$ub95 '{printf"%f",l+(u-l)/2}'`
-rad95=`echo "" | $AWK -v l=$lb95 -v u=$ub95 '{printf"%f",(u-l)/2}'`
-lb90=`cat $TMP/scef.sorted | head -$l90  | tail -1`;
-ub90=`cat $TMP/scef.sorted | head -$s90 | tail -1`;
-center90=`echo "" | $AWK -v l=$lb90 -v u=$ub90 '{printf"%f",l+(u-l)/2}'`
-rad90=`echo "" | $AWK -v l=$lb90 -v u=$ub90 '{printf"%f",(u-l)/2}'`
-
-echo "Confidence intervals";
-echo "conf.int.95 = [ $lb95 , $ub95 ] $center95 +- $rad95"
-echo "conf.int.90 = [ $lb90 , $ub90 ] $center90 +- $rad90"
-echo;
+# Output result of bootstrap evaluation [Effron93]
+echo "Bootstrap evaluation:"
+bootstrap_eval
+echo ""
--- utils/stack_dec/thot_conf_interv_smt.sh
+++ utils/stack_dec/thot_conf_interv_smt.sh
@@ -6,8 +6,95 @@
 
 # \textbf{Categ}: evaluation
 
-SCR=${0##*/};
+#####################################################################
+standard_eval()
+{
+    if [ $measure = "WER" ]; then
+        $bindir/thot_calc_wer -r $ref -t $test
+    else
+        $bindir/thot_calc_bleu -r $ref -t $test
+    fi
+}
 
+#####################################################################
+bootstrap_exper()
+{
+    # Initialize random seed
+    r=$RANDOM
+
+    # Generate test corpus
+    cat $test | \
+        $AWK -v seed=$r -v ref=$ref -v S=$S -v tmp=$TMP '
+         BEGIN{
+	       nr=0;
+               while(getline<ref) refh[++nr]=$0; 
+              }
+              {
+               testh[NR]=$0;
+              }
+           END{
+               srand(seed);
+               for(i=1;i<=S;i++)
+               {
+                r=rand();
+                id=1+int(NR*r);
+                printf "%s\n",testh[id] > tmp"/test";
+                printf "%s\n",refh[id] > tmp"/ref";
+               }
+              }'
+
+    # Compute measure for test corpus
+    if [ $measure = "WER" ]; then
+        a=`$bindir/thot_calc_wer -r $TMP/ref -t $TMP/test | head -1 | cut -d " " -f 2`
+    else
+        a=`$bindir/thot_calc_bleu -r $TMP/ref -t $TMP/test | head -1 | cut -d " " -f 2`
+    fi
+
+    # Output measure
+    echo $a
+}
+
+#####################################################################
+bootstrap_eval()
+{
+    # Perform bootstrap experiments
+    RANDOM=$seed
+    n=1
+    while [ $n -lt $N ]; do
+        bootstrap_exper >> $TMP/scef
+        n=`expr $n + 1`
+    done
+
+    # Obtain mean and stdev
+    mean=`cat $TMP/scef | $AWK '{sum+=$0;}END{printf "%f", sum/NR;}'`
+    echo "Mean: $mean"
+    stDev=`cat $TMP/scef | $AWK -v med=$mean '{aux=$0-med; stDev+=aux*aux;}END{printf "%f", sqrt(stDev/(NR-1));}'`
+    echo "stDev: $stDev"
+    echo ""
+
+    # Obtain confidence intervals
+    $SORT -n $TMP/scef > $TMP/scef.sorted;
+    l95=`echo "" | $AWK -v N=$N '{printf"%d",0.025*N+1}' `
+    l90=`echo "" | $AWK -v N=$N '{printf"%d",0.05*N+1}' `;
+    s95=`echo "" | $AWK -v N=$N -v l95=${l95} '{printf"%d",N-l95+1}' `;
+    s90=`echo "" | $AWK -v N=$N -v l90=${l90} '{printf"%d",N-l90+1}' `;
+    
+    lb95=`cat $TMP/scef.sorted | head -${l95}  | tail -1`;
+    ub95=`cat $TMP/scef.sorted | head -${s95} | tail -1`;
+    center95=`echo "" | $AWK -v l=$lb95 -v u=$ub95 '{printf"%f",l+(u-l)/2}'`
+    rad95=`echo "" | $AWK -v l=$lb95 -v u=$ub95 '{printf"%f",(u-l)/2}'`
+    lb90=`cat $TMP/scef.sorted | head -$l90  | tail -1`;
+    ub90=`cat $TMP/scef.sorted | head -$s90 | tail -1`;
+    center90=`echo "" | $AWK -v l=$lb90 -v u=$ub90 '{printf"%f",l+(u-l)/2}'`
+    rad90=`echo "" | $AWK -v l=$lb90 -v u=$ub90 '{printf"%f",(u-l)/2}'`
+    
+    # Print results
+    echo "Confidence intervals";
+    echo "conf.int.95 = [ $lb95 , $ub95 ] $center95 +- $rad95"
+    echo "conf.int.90 = [ $lb90 , $ub90 ] $center90 +- $rad90"
+}
+
+#####################################################################
 if [ $# -ne 6 ]; then 
     echo "Computes significance tests"
     echo ""
@@ -21,11 +108,8 @@ if [ $# -ne 6 ]; then
     exit 1; 
 fi
 
+# Initialize variables
 export LC_ALL=C;
-
-TMP=${TMPDIR:-/tmp}/$SCR.$$
-trap "rm -rf $TMP* 2>/dev/null" EXIT
-mkdir $TMP
 
 seed=$1    # Random seed
 ref=$2;    # File with references
@@ -45,73 +129,20 @@ if [ ! -f $test ]; then
     exit 1
 fi
 
+# Create directory for temporary files
+TMP=`mktemp -d`
+trap "rm -rf $TMP* 2>/dev/null" EXIT
+
 # Print random seed
 echo "Random seed: $seed"
 echo;
 
-# Standard evaluation
+# Output result of standard evaluation
 echo "Standard evaluation:"
-if [ $measure = "WER" ]; then
-    $bindir/thot_calc_wer -r $ref -t $test
-else
-    $bindir/thot_calc_bleu -r $ref -t $test
-fi
-echo;
+standard_eval
+echo ""
 
-# Bootstrap Effron93
-RANDOM=$seed
-for ((n=1;n<=N;n++)); do
-    r=$RANDOM
-    cat $test | \
-        $AWK -v seed=$r -v ref=$ref -v S=$S -v tmp=$TMP '
-    BEGIN{
-	  nr=0;
-          while(getline<ref) refh[++nr]=$0; 
-	 }
-    {testh[NR]=$0;}
-    END{
-        srand(seed);
-        for(i=1;i<=S;i++)
-        {
-         r=rand();
-         id=1+int(NR*r);
-         printf "%s\n",testh[id] > tmp"/test";
-         printf "%s\n",refh[id] > tmp"/ref";
-        }
-       }'
-    if [ $measure = "WER" ]; then
-        a=`$bindir/thot_calc_wer -r $TMP/ref -t $TMP/test | head -1 | cut -d " " -f 2`
-    else
-        a=`$bindir/thot_calc_bleu -r $TMP/ref -t $TMP/test | head -1 | cut -d " " -f 2`
-    fi
-
-    echo $a >> $TMP/scef;
-done
-
-# Mean and stDev
-mean=`cat $TMP/scef | $AWK '{sum+=$0;}END{printf "%f", sum/NR;}'`
-echo "Mean: $mean"
-stDev=`cat $TMP/scef | $AWK -v med=$mean '{aux=$0-med; stDev+=aux*aux;}END{printf "%f", sqrt(stDev/(NR-1));}'`
-echo "stDev: $stDev"
-echo;
-
-# Confidence interval
-sort -n $TMP/scef > $TMP/scef.sorted;
-l95=`echo "" | $AWK -v N=$N '{printf"%d",0.025*N+1}' `
-l90=`echo "" | $AWK -v N=$N '{printf"%d",0.05*N+1}' `;
-s95=`echo "" | $AWK -v N=$N -v l95=${l95} '{printf"%d",N-l95+1}' `;
-s90=`echo "" | $AWK -v N=$N -v l90=${l90} '{printf"%d",N-l90+1}' `;
-
-lb95=`cat $TMP/scef.sorted | head -${l95}  | tail -1`;
-ub95=`cat $TMP/scef.sorted | head -${s95} | tail -1`;
-center95=`echo "" | $AWK -v l=$lb95 -v u=$ub95 '{printf"%f",l+(u-l)/2}'`
-rad95=`echo "" | $AWK -v l=$lb95 -v u=$ub95 '{printf"%f",(u-l)/2}'`
-lb90=`cat $TMP/scef.sorted | head -$l90  | tail -1`;
-ub90=`cat $TMP/scef.sorted | head -$s90 | tail -1`;
-center90=`echo "" | $AWK -v l=$lb90 -v u=$ub90 '{printf"%f",l+(u-l)/2}'`
-rad90=`echo "" | $AWK -v l=$lb90 -v u=$ub90 '{printf"%f",(u-l)/2}'`
-
-echo "Confidence intervals";
-echo "conf.int.95 = [ $lb95 , $ub95 ] $center95 +- $rad95"
-echo "conf.int.90 = [ $lb90 , $ub90 ] $center90 +- $rad90"
-echo;
+# Output result of bootstrap evaluation [Effron93]
+echo "Bootstrap evaluation:"
+bootstrap_eval
+echo ""
