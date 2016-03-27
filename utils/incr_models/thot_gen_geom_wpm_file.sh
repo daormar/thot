1cccf14618bb49247ba97f53f77b8db18609331b
daormar <daormar2@gmail.com>
2016-03-27 15:33:29 +0200

    Incorporate thot_gen_geom_wpm_file tool.

---
+++ utils/incr_models/thot_gen_geom_wpm_file.sh
@@ -0,0 +1,11 @@
+# Author: Daniel Ortiz Mart\'inez
+# *- bash -*
+
+# Generates a geometric word penalty model file given the value of p.
+
+if [ $# -ne 1 ]; then
+    echo "Usage: thot_gen_geom_wp_file <p>"
+else
+    echo "Geometric wp model file..."
+    echo $1
+fi
