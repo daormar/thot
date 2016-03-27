5d95a9fd8a7ccc97150c0b41304499b6be7d634b
daormar <daormar2@gmail.com>
2016-03-27 16:02:35 +0200

    Correct help message.

--- utils/incr_models/thot_gen_geom_wpm_file.sh
+++ utils/incr_models/thot_gen_geom_wpm_file.sh
@@ -4,7 +4,7 @@
 # Generates a geometric word penalty model file given the value of p.
 
 if [ $# -ne 1 ]; then
-    echo "Usage: thot_gen_geom_wp_file <p>"
+    echo "Usage: thot_gen_geom_wpm_file <p>"
 else
     echo "Geometric wp model file..."
     echo $1
