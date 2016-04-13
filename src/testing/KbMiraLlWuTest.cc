e7b440dc7178ed26b9efd34411bd18902a98ec28
Daniel Ortiz <daniel.o@webinterpret.com>
2016-04-13 20:51:33 +0200

    Add code testing functionality.

---
+++ src/testing/KbMiraLlWuTest.cc
@@ -0,0 +1,61 @@
+/*
+thot package for statistical machine translation
+Copyright (C) 2013 Daniel Ortiz-Mart\'inez
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/********************************************************************/
+/*                                                                  */
+/* Module: KbMiraLlWuTest                                           */
+/*                                                                  */
+/* Definitions file: KbMiraLlWuTest.cc                              */
+/*                                                                  */
+/********************************************************************/
+
+
+//--------------- Include files --------------------------------------
+
+#include "KbMiraLlWuTest.h"
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( KbMiraLlWuTest );
+
+//--------------- KbMiraLlWuTest class functions
+//
+
+//---------------------------------------
+void KbMiraLlWuTest::setUp()
+{
+}
+
+//---------------------------------------
+void KbMiraLlWuTest::tearDown()
+{
+}
+
+//---------------------------------------
+void KbMiraLlWuTest::testFeature()
+{
+  //     // Set up
+  // const std::string currencyFF( "FF" );
+  // const double longNumber = 12345678.90123;
+
+  //     // Process
+  // Money money( longNumber, currencyFF );
+
+  //     // Check
+  // CPPUNIT_ASSERT_EQUAL( longNumber, money.getAmount() );
+  // CPPUNIT_ASSERT_EQUAL( currencyFF, money.getCurrency() );
+}
