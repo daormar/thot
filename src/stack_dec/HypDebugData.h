2346ee1ae800553213650a06d08735fd8cfe7171
Daniel Ortiz <daniel.o@webinterpret.com>
2016-04-13 16:07:41 +0200

    Add HypDebugData class files.

---
+++ src/stack_dec/HypDebugData.h
@@ -0,0 +1,76 @@
+/*
+thot package for statistical machine translation
+Copyright (C) 2013 Daniel Ortiz-Mart\'inez
+ 
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+ 
+/********************************************************************/
+/*                                                                  */
+/* Module: HypDebugData                                             */
+/*                                                                  */
+/* Prototypes file: HypDebugData.h                                  */
+/*                                                                  */
+/* Description: Class for debugging purposes.                       */
+/*                                                                  */
+/********************************************************************/
+
+#ifndef _HypDebugData_h
+#define _HypDebugData_h
+
+//--------------- Include files --------------------------------------
+
+#include "myVector.h"
+#include "Score.h"
+#include <string.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+//--------------- Constants ------------------------------------------
+
+
+//--------------- Classes --------------------------------------------
+
+
+//--------------- HypDebugData class: class for storing debugging
+//                                    information of each operation
+//                                    made during the expansion process
+
+class HypDebugData
+{
+ public:
+
+  std::string opCode;
+  Vector<unsigned int> parameters;
+  Vector<Score> partialContribs;
+  Score accum;
+
+  void print(ostream &outS)const
+    {
+      outS<<"Op: "<<opCode;
+      for(unsigned int i=0;i<parameters.size();++i)
+      {
+        outS<<" "<<parameters[i];
+      }
+      outS<<" ;";
+      for(unsigned int i=0;i<partialContribs.size();++i)
+      {
+        outS<<" "<<partialContribs[i];
+      }
+      outS<<" ; Accum. Score: "<<accum<<endl;
+    }
+};
+
+#endif
