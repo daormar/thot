b87f9edf14bb31877c4b46df43f897a235c91537
Daniel Ortiz <daniel.o@webinterpret.com>
2016-04-13 20:21:12 +0200

    Correct some code comments.

--- src/stack_dec/KbMiraLlWu.cc
+++ src/stack_dec/KbMiraLlWu.cc
@@ -18,9 +18,9 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 
 /********************************************************************/
 /*                                                                  */
-/* Module: BatchKbMiraWu                                            */
+/* Module: KbMiraLlWu                                               */
 /*                                                                  */
-/* Definitions file: BatchKbMiraWu.cc                               */
+/* Definitions file: KbMiraLlWu.cc                                  */
 /*                                                                  */
 /********************************************************************/
 
@@ -38,7 +38,7 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 #include <iostream>
 
 
-//--------------- KbMiraWu class functions
+//--------------- KbMiraLlWu class functions
 //
 
 //---------------------------------------
