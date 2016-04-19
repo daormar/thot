4e9cc52e79e0bad0413a7af4d4fb334db4023dfb
Daniel Ortiz <daniel.o@webinterpret.com>
2016-04-19 12:38:08 +0200

    Improve robustness in readStringSeq() function.

--- src/nlp_common/options.cc
+++ src/nlp_common/options.cc
@@ -530,23 +530,23 @@ int readStringSeq(int argc,
     {
       Vector<string> strVecAux;
       bool end=false;
+      ++i;
       while(!end)
       {
-        ++i;
-        strVecAux.push_back(argv[i]);
-        if(i==argc-1) end=true;
+        if(i>=argc) end=true;
         else
         {
-          if(isOption(argv[i+1]))
-          {
+          if(isOption(argv[i]))
             end=true;
-          }
+          else
+            strVecAux.push_back(argv[i]);
         }
+        ++i;
       }
       strVec=strVecAux;
       return 0;
     }
-    i++;
+    ++i;
   }
   return -1;   
 }
