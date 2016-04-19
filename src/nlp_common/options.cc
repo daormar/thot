a983e36f5fa467e22e892a75d805e9e70c83cb9a
Daniel Ortiz <daniel.o@webinterpret.com>
2016-04-19 12:59:39 +0200

    Improve robustness of readFloatSeq() function.

--- src/nlp_common/options.cc
+++ src/nlp_common/options.cc
@@ -234,29 +234,30 @@ int readFloatSeq(int argc,
                  const char *pszLabel,
                  Vector<float>& floatVec)
 {
-  int i;
   float value;
-  
-  i = 1;
+  int i = 1;
+
   while (i < argc-1)
   {
     if (!strcmp(argv[i], pszLabel))
     {
       Vector<float> floatVecAux;
       bool end=false;
+      ++i;
       while(!end)
       {
-        ++i;
-        sscanf(argv[i], "%f", &value);
-        floatVecAux.push_back(value);
-        if(i==argc-1) end=true;
+        if(i>=argc) end=true;
         else
         {
-          if(isOption(argv[i+1]))
-          {
+          if(isOption(argv[i]))
             end=true;
+          else
+          {
+            sscanf(argv[i], "%f", &value);
+            floatVecAux.push_back(value);
           }
         }
+        ++i;
       }
       floatVec=floatVecAux;
       return 0;
@@ -524,6 +525,7 @@ int readStringSeq(int argc,
                  Vector<string>& strVec)
 {
   int i = 1;
+  
   while (i < argc-1)
   {
     if (!strcmp(argv[i], pszLabel))
