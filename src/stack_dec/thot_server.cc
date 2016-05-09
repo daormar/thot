8e78b99af75770d44e9bcd22607b63dbe619d3f8
daormar <dortiz@prhlt.upv.es>
2016-05-09 13:35:45 +0200

    Add missing #include directive.

--- src/stack_dec/thot_server.cc
+++ src/stack_dec/thot_server.cc
@@ -29,6 +29,10 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 
 //--------------- Include files --------------------------------------
 
+#if HAVE_CONFIG_H
+#  include <thot_config.h>
+#endif /* HAVE_CONFIG_H */
+
 #include "ThotDecoder.h"
 #include <ErrorDefs.h>
 #include <StrProcUtils.h>
