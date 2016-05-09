28cff75d6a84a2afe22379b5df977a22ad9fa17a
daormar <dortiz@prhlt.upv.es>
2016-05-09 11:25:17 +0200

    Add -p option.

--- utils/nlp_common/thot_handle_ini_files.sh
+++ utils/nlp_common/thot_handle_ini_files.sh
@@ -2,21 +2,25 @@
 # *- bash -*
 
 if [ $# -lt 1 ]; then
-    echo "Use: thot_handle_ini_files [-s|-w <string>|-r]"
+    echo "Use: thot_handle_ini_files [-s|-w <string>|-r|-p<int>]"
     echo ""
     echo "-s          : Show content of master.ini file"
     echo "-w <string> : Overwrite master.ini with the file provided as argument."
     echo "              NOTE: this action may require superuser privileges depending"
     echo "              on where the package was installed"
     echo "-r          : Revert master.ini to its original content after installing"
-    echo "              the package"
+    echo "              the package (standard.ini)"
+    echo "-p <int>    : Use predefined configuration."
+    echo "              0 -> Standard configuration (standard.ini)"
+    echo "              1 -> Standard configuration with IBM 2 word alignment models"
+    echo "                   (standard_ibm2.ini)"
     echo ""
-else
-    
+else    
     # Read parameters
     s_given=0
     w_given=0
     r_given=0
+    p_given=0
     num_opts=0
     while [ $# -ne 0 ]; do
         case $1 in
@@ -33,11 +37,23 @@ else
             "-r")  r_given=1
                 num_opts=`expr $num_opts + 1`
                 ;;
+            "-p") shift
+                if [ $# -ne 0 ]; then
+                    p_given=1
+                    requested_config=$1
+                    num_opts=`expr $num_opts + 1`
+                fi
+                ;;
         esac
         shift
     done
 
     # Check parameters
+    if [ $num_opts -eq 0 ]; then
+        echo "Error: invalid parameters" >&2
+        exit 1
+    fi
+
     if [ $num_opts -gt 1 ]; then
         echo "Error: only one option can be given simultaneously" >&2
         exit 1
@@ -84,4 +100,25 @@ else
         echo "Done" >&2
     fi
 
+    if [ $p_given -eq 1 ]; then
+        valid_config=1
+        case ${requested_config} in
+            0)
+                file=${datadir}/ini_files/standard.ini
+                ;;
+            1)
+                file=${datadir}/ini_files/standard_ibm2.ini
+                ;;
+            *)
+                valid_config=0
+                ;;
+        esac
+        if [ ${valid_config} -eq 1 ]; then
+            echo "Overwriting current master.ini file with $file..." >&2
+            cp $file ${datadir}/ini_files/master.ini || exit 1
+            echo "Done" >&2
+        else
+            echo "Error: requested configuration code (${requested_config}) not valid" >&2
+        fi    
+    fi
 fi
