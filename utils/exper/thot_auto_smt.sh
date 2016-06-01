56e996ecdd28a739585c5fe8f5d5daf24a16ffc1
daormar <dortiz@prhlt.upv.es>
2016-06-01 18:13:49 +0200

    Print additional parameters to input_pars.txt.

--- utils/exper/thot_auto_smt.sh
+++ utils/exper/thot_auto_smt.sh
@@ -486,6 +486,8 @@ fi
 ## Print parameters
 echo "-s is ${scorpus_pref}" > ${outd}/input_pars.txt
 echo "-t is ${tcorpus_pref}" >> ${outd}/input_pars.txt
+echo "-n is ${n_val}" >> ${outd}/input_pars.txt
+echo "-nit is ${nitval}" >> ${outd}/input_pars.txt
 echo "--skip-clean is ${skip_clean_given}" >> ${outd}/input_pars.txt
 echo "--tok is ${tok_given}" >> ${outd}/input_pars.txt
 echo "--lower is ${lower_given}" >> ${outd}/input_pars.txt
