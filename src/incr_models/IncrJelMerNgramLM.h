75f9e4bf399d5c1be45208b915efd2462862c593
daormar <dortiz@prhlt.upv.es>
2016-08-25 16:04:09 +0200

    Correct code comment.

--- src/incr_models/IncrJelMerNgramLM.h
+++ src/incr_models/IncrJelMerNgramLM.h
@@ -18,7 +18,7 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
  
 /********************************************************************/
 /*                                                                  */
-/* Module: IncrInterpONgramLM                                       */
+/* Module: IncrJelMerNgramLM                                        */
 /*                                                                  */
 /* Prototype file: IncrJelMerNgramLM.h                              */
 /*                                                                  */
