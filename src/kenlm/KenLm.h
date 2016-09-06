57de72574963fdb55f394086dde702e3cd0fa164
daormar <dortiz@prhlt.upv.es>
2016-09-06 19:32:20 +0200

    Reintroduce include directive for thot_config.h

--- src/kenlm/KenLm.h
+++ src/kenlm/KenLm.h
@@ -31,9 +31,9 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 
 //--------------- Include files --------------------------------------
 
-/* #if HAVE_CONFIG_H */
-/* #  include <thot_config.h> */
-/* #endif /\* HAVE_CONFIG_H *\/ */
+#if HAVE_CONFIG_H
+#  include <thot_config.h>
+#endif /* HAVE_CONFIG_H */
 
 #include "lm/model.hh"
 #include "BaseNgramLM.h"
