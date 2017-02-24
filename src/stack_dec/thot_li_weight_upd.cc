67bc15a62d3d62c147c85b5781b4b04caceceaad
daormar <dortiz@prhlt.upv.es>
2017-02-24 16:34:23 +0100

    Correct code comment.

--- src/stack_dec/thot_li_weight_upd.cc
+++ src/stack_dec/thot_li_weight_upd.cc
@@ -1,5 +1,5 @@
 /*
-incr_models package for statistical machine translation
+thot package for statistical machine translation
 Copyright (C) 2013 Daniel Ortiz-Mart\'inez
  
 This library is free software; you can redistribute it and/or
