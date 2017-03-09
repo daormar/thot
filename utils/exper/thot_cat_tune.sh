915c4d3215ccbb82f79cb2fd38dfc4e822de600a
daormar <dortiz@prhlt.upv.es>
2017-03-09 13:03:42 +0100

    Modify code related to server weights.
    Number of weights for different models are stored in variables.

--- utils/exper/thot_cat_tune.sh
+++ utils/exper/thot_cat_tune.sh
@@ -317,10 +317,19 @@ create_cfg_file_for_tuning()
 }
 
 ##################
+print_weight_info()
+{
+    ${bindir}/thot_server -c ${outd}/tune_loglin.cfg -w 2> /dev/null
+}
+
+##################
 obtain_smtweights_names()
 {
     # local_line=`${bindir}/thot_get_ll_weights | $GREP "\- SMT model weights="`
-    local_line=`${bindir}/thot_server -c ${outd}/tune_loglin.cfg -w 2> /dev/null | $GREP "\- SMT model weights="`
+    if [ ! -f $outd/server_weights ]; then
+        print_weight_info > $outd/server_weights
+    fi
+    local_line=`$GREP "\- SMT model weights=" $outd/server_weights`
     local_smtw_names=`echo ${local_line} | $AWK '{for(i=5;i<=NF;i+=3) printf"%s ",substr($i,1,length($i)-1)}'`
     echo ${local_smtw_names}
 }
@@ -329,7 +338,10 @@ obtain_smtweights_names()
 obtain_ecmweights_names()
 {
     # local_line=`${bindir}/thot_get_ll_weights | $GREP "\- Error correction model weights="`
-    local_line=`${bindir}/thot_server -c ${outd}/tune_loglin.cfg -w 2> /dev/null | $GREP "\- Error correction model weights="`
+    if [ ! -f $outd/server_weights ]; then
+        print_weight_info > $outd/server_weights
+    fi
+    local_line=`$GREP "\- Error correction model weights=" $outd/server_weights`
     local_ecmw_names=`echo ${local_line} | $AWK '{for(i=6;i<=NF;i+=3) printf"%s ",substr($i,1,length($i)-1)}'`
     echo ${local_ecmw_names}
 }
@@ -338,7 +350,10 @@ obtain_ecmweights_names()
 obtain_catweights_names()
 {
     # local_line=`${bindir}/thot_get_ll_weights | $GREP "\- Assisted translator weights="`
-    local_line=`${bindir}/thot_server -c ${outd}/tune_loglin.cfg -w 2> /dev/null | $GREP "\- Assisted translator weights="`
+    if [ ! -f $outd/server_weights ]; then
+        print_weight_info > $outd/server_weights
+    fi
+    local_line=`$GREP "\- Assisted translator weights=" $outd/server_weights`
     local_catw_names=`echo ${local_line} | $AWK '{for(i=5;i<=NF;i+=3) printf"%s ",substr($i,1,length($i)-1)}'`
     echo ${local_catw_names}
 }
@@ -399,6 +414,11 @@ loglin_downhill()
     export SERVER_IP="127.0.0.1"
     export PORT=$RANDOM
     export QS="${qs_par}"
+
+    # Obtain number of weights for each model
+    export NSMTW=`num_smtw`
+    export NCATW=`num_catw`
+    export NECW=`expr $NUMW - $NSMTW - $NCATW`
 
     # Generate information for weight initialisation
     export NON_NEG_CONST=`obtain_loglin_nonneg_const`
