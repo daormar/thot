b0585baeac71f2b6a6129c6fc3d16a0639a9c2cc
daormar <dortiz@prhlt.upv.es>
2017-03-09 11:31:21 +0100

    Modify info printed to error output.

--- utils/stack_dec/thot_ll_weight_upd.sh
+++ utils/stack_dec/thot_ll_weight_upd.sh
@@ -90,7 +90,7 @@ get_sentid()
 ##################
 obtain_cfg_llweights()
 {
-    local_line=`$bindir/thot_server -c $cfgfile -w | $HEAD -1`
+    local_line=`$bindir/thot_server -c $cfgfile -w 2>/dev/null | $HEAD -1`
     local_cfg_llw_names=`echo ${local_line} | $AWK '{for(i=5;i<=NF;i+=3) printf"%s ",substr($i,1,length($i)-1)}'`
     local_cfg_llw_values=`echo ${local_line} | $AWK '{for(i=6;i<=NF;i+=3) printf"%s ",$i}'`
     echo ${local_cfg_llw_values}
