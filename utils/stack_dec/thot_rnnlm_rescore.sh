898e485ae0a911720d308de3e110f7e22b213082
daormar <dortiz@prhlt.upv.es>
2017-04-04 11:50:30 +0200

    Correct handling of -removelm option.

--- utils/stack_dec/thot_rnnlm_rescore.sh
+++ utils/stack_dec/thot_rnnlm_rescore.sh
@@ -24,18 +24,17 @@ version()
 ########
 usage()
 {
-    echo "thot_rnnlm_rescore      -t <string> -n <string> -r <string>"
-    echo "                        -o <string> [-voc <string>] [-removelm]"
+    echo "thot_rnnlm_rescore      -n <string> -r <string> -o <string>"
+    echo "                        [-voc <string>] [-removelm]"
     echo "                        [-tdir <string>] [-debug] [--help] [--version]"
     echo ""
-    echo "-t <string>             File with target sentences."
     echo "-n <string>             Directory containing n-best lists."
     echo "-r <string>             RNN LM file."
     echo "-o <string>             Output directory common to all processors."
     echo "-voc <string>           File with training vocabulary. Words occuring in"
     echo "                        the n-best lists outside vocabulary are replaced by"
     echo "                        <unk>."
-    echo "--removelm              Remove previous language model component (lm) from"
+    echo "-removelm              Remove previous language model component (lm) from"
     echo "                        n-best lists."
     echo "-tdir <string>          Directory for temporary files (/tmp by default)."
     echo "                        NOTES:"
@@ -55,17 +54,6 @@ get_sentid()
 }
 
 ########
-nblist_voc_filter()
-{
-    # Handle -voc option
-    if [ ${voc_given} -eq 0 ]; then
-        cat
-    else
-        ${bindir}/thot_replace_oov_with_unk -v ${vocab}
-    fi    
-}
-
-########
 nblist_removelm_filter()
 {
     # Handle -removelm option
@@ -80,7 +68,25 @@ nblist_removelm_filter()
 process_nblist()
 {
     local_nblist=$1
-    cat ${local_nblist} | nblist_voc_filter | nblist_removelm_filter
+    cat ${local_nblist} | nblist_removelm_filter
+}
+
+########
+trans_voc_filter()
+{
+    # Handle -voc option
+    if [ ${voc_given} -eq 0 ]; then
+        cat
+    else
+        ${bindir}/thot_replace_oov_with_unk -v ${vocab}
+    fi    
+}
+
+########
+process_trans()
+{
+    local_nblist=$1
+    cat ${local_nblist} | trans_voc_filter
 }
 
 ########
@@ -93,7 +99,7 @@ compute_rnnlm_scores_for_nblists()
         ${bindir}/thot_extract_sents_from_nbl < $nblfile > ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans_aux
 
         # Process n-best list
-        process_nblist ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans_aux > ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans
+        process_trans ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans_aux > ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans
 
         # Remove temporary files
         rm ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans_aux
@@ -102,7 +108,7 @@ compute_rnnlm_scores_for_nblists()
         ${FASTER_RNNLM_BUILD_DIR}/rnnlm -rnnlm ${rnnlm} \
             -test ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans \
             > ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.rnnlm_scores 2> ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.log
-    done
+    done || return 1
 }
 
 ########
@@ -115,14 +121,14 @@ fast_compute_rnnlm_scores_for_nblists()
         ${bindir}/thot_extract_sents_from_nbl < $nblfile > ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans_aux
 
         # Process n-best list
-        process_nblist ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans_aux > ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans
+        process_trans ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans_aux > ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans
 
         # Remove temporary files
         rm ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans_aux
 
         # Add translations to all translations file
         cat ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans >> ${TDIR_RNNLM_RESCORE}/rnnlm_scores/all_trans
-    done
+    done || return 1
 
     # Score all translations file
     ${FASTER_RNNLM_BUILD_DIR}/rnnlm -rnnlm ${rnnlm} \
@@ -137,7 +143,7 @@ fast_compute_rnnlm_scores_for_nblists()
         numtrans=`$WC -l ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.trans | $AWK '{printf"%s",$1}'`
         offset=`expr $offset + $numtrans`
         $HEAD -${offset} $scrfile | $TAIL -${numtrans} > ${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.rnnlm_scores
-    done
+    done || return 1
 }
 
 ########
@@ -145,101 +151,17 @@ create_aug_nblists()
 {
     # Process n-best lists
     for nblfile in ${nblistdir}/*.nbl; do
+        # Process n-best list
         nblfile_base=`${BASENAME} $nblfile`
+        process_nblist $nblfile > ${nblfile}_aux
+
+        # Augment n-best list
         file_with_new_score=${TDIR_RNNLM_RESCORE}/rnnlm_scores/${nblfile_base}.rnnlm_scores
-        ${bindir}/thot_augment_nblist -n $nblfile -a rnnlm -s ${file_with_new_score} > ${TDIR_RNNLM_RESCORE}/augmented_nblists/${nblfile_base}
-    done
-}
+        ${bindir}/thot_augment_nblist -n ${nblfile}_aux -a rnnlm -s ${file_with_new_score} > ${outd}/${nblfile_base}
 
-########
-obtain_loglin_nonneg_const()
-{
-    _smtw_names=`obtain_smtweights_names`
-    echo "${_smtw_names}" | $AWK '{for(i=1;i<=NF;++i) if($i=="wpw" || $i=="tseglenw") printf"0 "; else printf"1 "}'
-}
-
-##################
-obtain_smtweights_names()
-{
-    # Process n-best lists
-    for nblfile in ${TDIR_RNNLM_RESCORE}/augmented_nblists/*.nbl; do
-        head -1 $nblfile | $AWK -F " , " '{for(i=1;i<=NF;++i){nf=split($i,winfo," "); printf"%s ",winfo[nf-1]}}'
-        break
-    done
-}
-
-##################
-obtain_smtweights_values()
-{
-    # Process n-best lists
-    for nblfile in ${TDIR_RNNLM_RESCORE}/augmented_nblists/*.nbl; do
-        head -1 $nblfile | $AWK -F " , " '{for(i=1;i<=NF;++i){nf=split($i,winfo," "); printf"%s ",winfo[nf]}}'
-        break
-    done
-}
-
-########
-obtain_loglin_dhs_va_opt_values()
-{
-    _smtw_names=`obtain_smtweights_names`
-#    echo "${_smtw_names}" | $AWK '{for(i=1;i<=NF;++i) if($i=="swlenliw") printf"0 "; else printf"-0 "}'
-    echo "${_smtw_names}" | $AWK '{for(i=1;i<=NF;++i) printf"-0 "}'
-}
-
-########
-obtain_loglin_iv_opt_values()
-{
-    _smtw_names=`obtain_smtweights_names`
-    echo "${_smtw_names}" | $AWK '{for(i=1;i<=NF;++i) printf"1 "}'
-}
-
-########
-loglin_downhill()
-{
-    # Export required variables
-    export NBL_DIR=${TDIR_RNNLM_RESCORE}/augmented_nblists
-    export REF=$tcorpus
-
-    # Generate information for weight initialisation
-    export NON_NEG_CONST="`obtain_loglin_nonneg_const`"
-    va_opt="`obtain_loglin_dhs_va_opt_values`"
-    iv_opt="`obtain_loglin_iv_opt_values`"
-
-    # Execute tuning algorithm
-    tdir=/tmp
-    ${bindir}/thot_dhs_min -tdir $tdir -va ${va_opt} -iv ${iv_opt} \
-        -ftol ${ftol_loglin} -o ${outd}/llweights_tune -u ${bindir}/thot_dhs_nbl_rescore ${debug_opt} || return 1
-}
-
-########
-obtain_loglin_upd_va_opt_values()
-{
-    _smtw_names=`obtain_smtweights_names`
-    echo "${_smtw_names}" | $AWK '{for(i=1;i<=NF;++i) printf"1 "}'
-}
-
-########
-loglin_upd()
-{
-    # Obtain file with n-best lists file names
-    for nblfile in ${TDIR_RNNLM_RESCORE}/augmented_nblists/*.nbl; do
-        echo $nblfile >> ${TDIR_RNNLM_RESCORE}/augmented_nblists/nbl_filenames.txt
-    done
-
-    # Obtain weight related information
-    llweights="`obtain_smtweights_values`"
-    va_opt="`obtain_loglin_upd_va_opt_values`"
-    
-    # Update weights given n-best lists
-    $bindir/thot_ll_weight_upd_nblist -w ${llweights} -va ${va_opt} \
-                                      -nb ${TDIR_RNNLM_RESCORE}/augmented_nblists/nbl_filenames.txt \
-                                      -r ${tcorpus} > ${outd}/llweights_tune.out 2>${outd}/llweights_tune.log || return 1
-
-    # Obtain score with optimized weights
-    export NBL_DIR=${TDIR_RNNLM_RESCORE}/augmented_nblists
-    export REF=$tcorpus
-    upd_weights="`cat ${outd}/llweights_tune.out | $AWK -F ": " '{print $2}'`"
-    ${bindir}/thot_dhs_nbl_rescore ${TDIR_RNNLM_RESCORE} ${upd_weights} > ${outd}/llweights_tune.scr
+        # Remove temporary file
+        rm ${nblfile}_aux
+    done || return 1
 }
 
 ##################
@@ -298,7 +220,7 @@ while [ $# -ne 0 ]; do
                 voc_given=1
             fi
             ;;
-        "-removelm") removelm=1
+        "-removelm") removelm_given=1
             ;;
         "-tdir") shift
             if [ $# -ne 0 ]; then
@@ -314,16 +236,6 @@ while [ $# -ne 0 ]; do
 done
 
 # Check parameters
-if [ ${t_given} -eq 0 ]; then        
-    echo "Error! -t parameter not given!" >&2
-    exit 1
-else
-    if [ ! -f ${tcorpus} ]; then
-        echo "Error! file ${tcorpus} does not exist" >&2
-        exit 1
-    fi
-fi
-
 if [ ${n_given} -eq 0 ]; then
     echo "Error! -n parameter not given!" >&2
     exit 1
@@ -361,10 +273,6 @@ if [ ${tdir_given} -eq 1 ]; then
     fi
 fi
 
-# Set default parameters
-ftol_lm=0.1
-ftol_loglin=0.001
-
 # Create tmp directory
 if [ ! -d ${tdir} ]; then
     echo "Error: tmp directory does not exist" >&2
@@ -380,7 +288,6 @@ fi
 
 # Create additional directories
 mkdir ${TDIR_RNNLM_RESCORE}/rnnlm_scores
-mkdir ${TDIR_RNNLM_RESCORE}/augmented_nblists
 
 # Compute rnnlm scores
 echo "* Computing rnn-lm scores for n-best lists" >&2
@@ -390,10 +297,4 @@ echo "" >&2
 # Create augmented n-best lists
 echo "* Creating augmented n-best lists" >&2
 create_aug_nblists || exit 1
-echo "" >&2
-
-# Update log-linear weights
-echo "* Updating log-linear weights for augmented n-best lists" >&2
-#loglin_downhill || exit 1
-loglin_upd || exit 1
 echo "" >&2
