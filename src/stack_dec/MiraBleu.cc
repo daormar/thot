c686296e5ae529109fa995831c14aa02ce6ef32c
daormar <dortiz@prhlt.upv.es>
2017-05-09 15:31:35 +0200

    Correct code comment.

--- src/stack_dec/MiraBleu.cc
+++ src/stack_dec/MiraBleu.cc
@@ -22,7 +22,7 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 #include "StrProcUtils.h"
 
 
-//--------------- KbMiraLlWu class functions
+//--------------- MiraBleu class functions
 
 //---------------------------------------
 double MiraBleu::scoreFromStats(Vector<unsigned int>& stats){
