92f238b186de6a36a93fa841a674707c6670d3be
daormar <dortiz@prhlt.upv.es>
2017-05-25 17:45:26 +0200

    Add new feature.

---
+++ src/stack_dec/TrgPhraseLenFeat.cc
@@ -0,0 +1,79 @@
+/*
+thot package for statistical machine translation
+Copyright (C) 2013 Daniel Ortiz-Mart\'inez
+ 
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+ 
+/********************************************************************/
+/*                                                                  */
+/* Module: TrgPhraseLenFeat                                         */
+/*                                                                  */
+/* Definitions file: TrgPhraseLenFeat.cc                            */
+/*                                                                  */
+/********************************************************************/
+
+
+//--------------- Include files --------------------------------------
+
+#include "TrgPhraseLenFeat.h"
+
+//--------------- TrgPhraseLenFeat class functions
+
+template<>
+typename TrgPhraseLenFeat<PhrScoreInfo>::HypScoreInfo
+TrgPhraseLenFeat<PhrScoreInfo>::extensionScore(const Vector<std::string>& srcSent,
+                                               const HypScoreInfo& predHypScrInf,
+                                               const PhrHypDataStr& predHypDataStr,
+                                               const PhrHypDataStr& newHypDataStr,
+                                               Score& unweightedScore)
+{
+      // Check if function was called to score the null hypothesis
+  if(predHypDataStr.sourceSegmentation.empty() && newHypDataStr.sourceSegmentation.empty())
+  {
+    return predHypScrInf;
+  }
+  else
+  {
+        // Obtain score for hypothesis extension
+    unsigned int trglen=predHypDataStr.ntarget.size()-1;
+    HypScoreInfo hypScrInf=predHypScrInf;
+    unweightedScore=0;
+    
+    for(unsigned int i=predHypDataStr.sourceSegmentation.size();i<newHypDataStr.sourceSegmentation.size();++i)
+    {
+          // Initialize variables
+      unsigned int trgLeft;
+      unsigned int trgRight=newHypDataStr.targetSegmentCuts[i];
+      if(i==0)
+        trgLeft=1;
+      else
+        trgLeft=newHypDataStr.targetSegmentCuts[i-1]+1;
+      unsigned int nextTrgPhraseLen=trgRight-trgLeft+1;
+
+          // Update score
+      Score iterScore=trgSegmLenScore(trglen+nextTrgPhraseLen,trglen,0);
+      unweightedScore+= iterScore;
+      hypScrInf.score+= weight*iterScore;
+      
+          // Increase trglen
+      trglen+=nextTrgPhraseLen;
+    }
+
+        // NOTE: There are no additional score contributions when the
+        // hypothesis is complete
+    
+    return hypScrInf;
+  }
+}
---
+++ src/stack_dec/TrgPhraseLenFeat.h
@@ -0,0 +1,137 @@
+/*
+thot package for statistical machine translation
+Copyright (C) 2013 Daniel Ortiz-Mart\'inez
+ 
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+ 
+/********************************************************************/
+/*                                                                  */
+/* Module: TrgPhraseLenFeat                                         */
+/*                                                                  */
+/* Prototypes file: TrgPhraseLenFeat.h                              */
+/*                                                                  */
+/* Description: Declares the TrgPhraseLenFeat template              */
+/*              class. This class implements a target phrase        */
+/*              length feature.                                     */
+/*                                                                  */
+/********************************************************************/
+
+/**
+ * @file TrgPhraseLenFeat.h
+ * 
+ * @brief Declares the TrgPhraseLenFeat template class. This class
+ * implements a target phrase length feature.
+ */
+
+#ifndef _TrgPhraseLenFeat_h
+#define _TrgPhraseLenFeat_h
+
+//--------------- Include files --------------------------------------
+
+#if HAVE_CONFIG_H
+#  include <thot_config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include "BasePhraseModel.h"
+#include "PhrScoreInfo.h"
+#include "BasePbTransModelFeature.h"
+
+//--------------- Constants ------------------------------------------
+
+
+//--------------- Classes --------------------------------------------
+
+//--------------- TrgPhraseLenFeat class
+
+/**
+ * @brief The TrgPhraseLenFeat template class is a base class for
+ * implementing a word penalty feature.
+ */
+
+template<class SCORE_INFO>
+class TrgPhraseLenFeat: public BasePbTransModelFeature<SCORE_INFO>
+{
+ public:
+
+  typedef typename BasePbTransModelFeature<SCORE_INFO>::HypScoreInfo HypScoreInfo;
+
+      // Constructor
+  TrgPhraseLenFeat();
+  
+      // Feature information
+  std::string getFeatType(void);
+
+      // Scoring functions
+  HypScoreInfo extensionScore(const Vector<std::string>& srcSent,
+                              const HypScoreInfo& predHypScrInf,
+                              const PhrHypDataStr& predHypDataStr,
+                              const PhrHypDataStr& newHypDataStr,
+                              Score& unweightedScore);
+  Score scorePhrasePair(const Vector<std::string>& srcPhrase,
+                        const Vector<std::string>& trgPhrase);
+
+      // Functions related to model pointers
+  void link_pm(BasePhraseModel* _invPbModelPtr);
+
+ protected:
+
+  BasePhraseModel* invPbModelPtr;
+
+  Score trgSegmLenScore(unsigned int x_k,
+                        unsigned int x_km1,
+                        unsigned int trgLen);
+};
+
+//--------------- TrgPhraseLenFeat class functions
+//
+
+template<class SCORE_INFO>
+TrgPhraseLenFeat<SCORE_INFO>::TrgPhraseLenFeat()
+{
+  this->weight=1;
+}
+
+//---------------------------------
+template<class SCORE_INFO>
+std::string TrgPhraseLenFeat<SCORE_INFO>::getFeatType(void)
+{
+  return "TrgPhraseLenFeat";
+}
+
+//---------------------------------
+template<class SCORE_INFO>
+Score TrgPhraseLenFeat<SCORE_INFO>::scorePhrasePair(const Vector<std::string>& /*srcPhrase*/,
+                                                    const Vector<std::string>& trgPhrase)
+{
+  return 0;
+}
+
+//---------------------------------
+template<class SCORE_INFO>
+void TrgPhraseLenFeat<SCORE_INFO>::link_pm(BasePhraseModel* _invPbModelPtr)
+{
+  invPbModelPtr=_invPbModelPtr;
+}
+
+//---------------------------------------
+template<class SCORE_INFO>
+Score TrgPhraseLenFeat<SCORE_INFO>::trgSegmLenScore(unsigned int x_k,
+                                                    unsigned int x_km1,
+                                                    unsigned int trgLen)
+{
+  return (double)invPbModelPtr->srcSegmLenLgProb(x_k,x_km1,trgLen);
+}
+
+#endif
