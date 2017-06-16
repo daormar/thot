182d23b8a5a4024122f73da0173ae1fbba4d28b3
daormar <dortiz@prhlt.upv.es>
2017-06-16 19:39:36 +0200

    Add new files.

---
+++ src/phrase_models/PhraseExtractUtils.h
@@ -0,0 +1,43 @@
+/*
+thot package for statistical machine translation
+Copyright (C) 2013 Daniel Ortiz-Mart\'inez
+ 
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+ 
+/**
+ * @file PhraseExtractUtils.h
+ * @brief Defines string processing utilities
+ */
+
+#ifndef _PhraseExtractUtils_h
+#define _PhraseExtractUtils_h
+
+#if HAVE_CONFIG_H
+#  include <thot_config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include "BaseSwAligModel.h"
+#include "BasePhraseModel.h"
+#include "PhrasePair.h"
+#include <stdio.h>
+#include "myVector.h"
+#include <string>
+
+namespace PhraseExtractUtils
+{
+  
+}
+
+#endif
