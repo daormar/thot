eb71235f203e84dea434c5e4ae6ed30dcacea87e
Adam Harasimowicz <adam.ha@webinterpret.com>
2017-07-13 16:08:53 +0200

    Increase size of prefixes stored in memory

--- src/phrase_models/LevelDbPhraseTable.cc
+++ src/phrase_models/LevelDbPhraseTable.cc
@@ -35,7 +35,7 @@ LevelDbPhraseTable::LevelDbPhraseTable(void)
 {
     options.create_if_missing = true;
     options.max_open_files = 4000;
-    options.filter_policy = leveldb::NewBloomFilterPolicy(16);
+    options.filter_policy = leveldb::NewBloomFilterPolicy(48);
     db = NULL;
     dbName = "";
 }
