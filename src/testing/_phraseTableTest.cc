f1c1c3a951ea9ed47238686f87018ce473b0371b
Adam Harasimowicz <adam.ha@webinterpret.com>
2017-09-05 13:03:45 +0200

    Extend unit test by checking returned item content

--- src/testing/_phraseTableTest.cc
+++ src/testing/_phraseTableTest.cc
@@ -118,16 +118,24 @@ void _phraseTableTest::testGetEntriesForTarget()
   result = tab->getEntriesForTarget(t1_1, node);
   CPPUNIT_ASSERT( result );
   CPPUNIT_ASSERT( node.size() == 2 );
+  CPPUNIT_ASSERT( node[s1_1].first.get_c_s() == 2 );
+  CPPUNIT_ASSERT( node[s1_1].second.get_c_st() == 1 );
+  CPPUNIT_ASSERT( node[s1_2].first.get_c_s() == 1 );
+  CPPUNIT_ASSERT( node[s1_2].second.get_c_st() == 1 );
 
   // Looking for phrases for which 'Pasleka' is translation
   result = tab->getEntriesForTarget(t1_2, node);
   CPPUNIT_ASSERT( result );
   CPPUNIT_ASSERT( node.size() == 1 );
+  CPPUNIT_ASSERT( node[s1_1].first.get_c_s() == 2);
+  CPPUNIT_ASSERT( node[s1_1].second.get_c_st() == 1 );
 
   // Looking for phrases for which 'rzeka' is translation
   result = tab->getEntriesForTarget(t2, node);
   CPPUNIT_ASSERT( result );
   CPPUNIT_ASSERT( node.size() == 1 );
+  CPPUNIT_ASSERT( node[s2].first.get_c_s() == 1);
+  CPPUNIT_ASSERT( node[s2].second.get_c_st() == 1 );
 
   // 'xyz'' key shoud not be found
   result = tab->getEntriesForTarget(getVector("xyz"), node);
