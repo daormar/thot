dc3372daa13f8fc4f6caff19449875868398dbb4
Adam Harasimowicz <adam.ha@webinterpret.com>
2017-09-27 16:53:39 +0200

    Add missing copyright information

--- src/stack_dec/thot_server.cc
+++ src/stack_dec/thot_server.cc
@@ -1,6 +1,6 @@
 /*
 thot package for statistical machine translation
-Copyright (C) 2013 Daniel Ortiz-Mart\'inez
+Copyright (C) 2013-2017 Daniel Ortiz-Mart\'inez, Adam Harasimowicz
  
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public License
