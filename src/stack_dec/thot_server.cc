d5d94cab53d008a431c67ebf4f1bbe39acb4ceef
daormar <dortiz@prhlt.upv.es>
2017-09-27 18:50:32 +0200

    Revise function naming.

--- src/stack_dec/thot_server.cc
+++ src/stack_dec/thot_server.cc
@@ -82,7 +82,7 @@ bool read_end_server_var(void);
 void set_end_server_var(void);
 void increase_num_threads_var(void);
 void decrease_num_threads_var(void);
-void wait_on_num_threads_var_cond(void);
+void wait_on_num_threads_var_cond_unlock(void);
 void sigchld_handler(int s);
 int handleParameters(int argc,
                      char *argv[]);
@@ -246,7 +246,7 @@ int start_server(void)
     std::cerr<<"Server: shutting down"<<std::endl;
 
       // Wait for threads to finish
-  wait_on_num_threads_var_cond();
+  wait_on_num_threads_var_cond_unlock();
   
       // Destroy mutexes and conditions
   pthread_mutex_destroy(&num_threads_var_mut);
@@ -312,7 +312,7 @@ void decrease_num_threads_var(void)
 }
 
 //---------------
-void wait_on_num_threads_var_cond(void)
+void wait_on_num_threads_var_cond_unlock(void)
 {
   pthread_mutex_lock(&num_threads_var_mut);
   /////////// begin of mutex
