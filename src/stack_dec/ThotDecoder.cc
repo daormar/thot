c46ce0214a599580abb5d53bc48054ec1a69da8d
daormar <dortiz@prhlt.upv.es>
2017-09-28 10:14:32 +0200

    Revise mutual exclusion mechanism.

--- src/stack_dec/ThotDecoder.cc
+++ src/stack_dec/ThotDecoder.cc
@@ -1203,11 +1203,6 @@ bool ThotDecoder::load_tm_feat_impl(const char* tmFilesPrefix,
                                     int verbose/*=0*/)
 {
   int ret;
-  // pthread_mutex_lock(&atomic_op_mut);
-  // /////////// begin of mutex 
-
-  //     // Wait until all non-atomic operations have finished
-  // wait_on_non_atomic_op_cond();
     
   if(strcmp(tdState.tmFilesPrefixGiven.c_str(),tmFilesPrefix)==0)
   {
@@ -1220,13 +1215,7 @@ bool ThotDecoder::load_tm_feat_impl(const char* tmFilesPrefix,
         // Store tm information
     if(ret==THOT_OK)
       tdState.tmFilesPrefixGiven=tmFilesPrefix;
-  }
-  
-  //     // Unlock non_atomic_op_cond mutex
-  // pthread_mutex_unlock(&non_atomic_op_mut);
-      
-  // /////////// end of mutex 
-  // pthread_mutex_unlock(&atomic_op_mut);
+  }  
 
   return ret;
 }
@@ -1331,6 +1320,9 @@ bool ThotDecoder::onlineTrainSentPair(int user_id,
   pthread_mutex_lock(&atomic_op_mut);
   /////////// begin of mutex 
 
+      // Wait until all non-atomic operations have finished
+  wait_on_non_atomic_op_cond();
+
       // Obtain index vector given user_id
   size_t idx=get_vecidx_for_user_id(user_id);
   if(verbose) std::cerr<<"user_id: "<<user_id<<", idx: "<<idx<<std::endl;
@@ -1410,6 +1402,9 @@ bool ThotDecoder::onlineTrainSentPair(int user_id,
     ctimer(&elapsedTime,&ucpu,&scpu);
     if(verbose) std::cerr<<"Training time: "<<elapsedTime-prevElapsedTime<<std::endl;
   }
+
+      // Unlock non_atomic_op_cond mutex
+  pthread_mutex_unlock(&non_atomic_op_mut);
 
   /////////// end of mutex 
   pthread_mutex_unlock(&atomic_op_mut);
@@ -1535,6 +1530,9 @@ bool ThotDecoder::trainEcm(int user_id,
   pthread_mutex_lock(&atomic_op_mut);
   /////////// begin of mutex 
 
+      // Wait until all non-atomic operations have finished
+  wait_on_non_atomic_op_cond();
+
       // Obtain index vector given user_id
   size_t idx=get_vecidx_for_user_id(user_id);
   if(verbose) std::cerr<<"user_id: "<<user_id<<", idx: "<<idx<<std::endl;
@@ -1562,6 +1560,9 @@ bool ThotDecoder::trainEcm(int user_id,
   {
     ret=tdCommonVars.ecModelPtr->trainStrPair(strx,stry,verbose);
   }
+
+      // Unlock non_atomic_op_cond mutex
+  pthread_mutex_unlock(&non_atomic_op_mut);
 
   /////////// end of mutex 
   pthread_mutex_unlock(&atomic_op_mut);
@@ -2073,6 +2074,9 @@ void ThotDecoder::clearTrans(int /*verbose*//*=0*/)
   pthread_mutex_lock(&atomic_op_mut);
   /////////// begin of mutex 
 
+      // Wait until all non-atomic operations have finished
+  wait_on_non_atomic_op_cond();
+
   tdCommonVars.wgHandlerPtr->clear();
   tdCommonVars.smtModelPtr->clear();
   tdCommonVars.ecModelPtr->clear();
@@ -2086,7 +2090,10 @@ void ThotDecoder::clearTrans(int /*verbose*//*=0*/)
   totalPrefixVec.clear();
   userIdToIdx.clear();
   idxDataReleased.clear();
-  
+
+      // Unlock non_atomic_op_cond mutex
+  pthread_mutex_unlock(&non_atomic_op_mut);
+
   /////////// end of mutex 
   pthread_mutex_unlock(&atomic_op_mut);
 }
