754894789dfc335f7ee5e2c062f5a6757299b1c8
daormar <dortiz@prhlt.upv.es>
2017-10-10 23:01:12 +0200

    Add missing include directive.

--- src/sw_models/thot_calc_swm_lgprob.cc
+++ src/sw_models/thot_calc_swm_lgprob.cc
@@ -30,6 +30,10 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 
 //--------------- Include files ---------------------------------------
 
+#if HAVE_CONFIG_H
+#  include <thot_config.h>
+#endif /* HAVE_CONFIG_H */
+
 #include "IncrHmmAligModel.h"
 #include "CachedHmmAligLgProb.h"
 #include "BaseSwAligModel.h"
