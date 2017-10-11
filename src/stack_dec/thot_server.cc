aeed2e45945a6c42cf171c3b31bcbf306b44613f
daormar <dortiz@prhlt.upv.es>
2017-10-11 09:21:10 +0200

    Avoid namespace ambiguities with bind.

--- src/stack_dec/thot_server.cc
+++ src/stack_dec/thot_server.cc
@@ -178,7 +178,7 @@ int start_server(thot_server_pars ts_pars,
   my_addr.sin_addr.s_addr = INADDR_ANY;  // fill with IP address
   memset(&(my_addr.sin_zero), '\0', 8);  // reset the rest of the data structure
 
-  if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))== -1)
+  if (::bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))== -1)
   {
     perror("bind");
     exit(1);
