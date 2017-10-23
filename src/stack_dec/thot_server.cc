ee8b7c417252626d5f45a79b2d8793b54f1e337e
daormar <dortiz@prhlt.upv.es>
2017-10-23 15:18:54 +0200

    Solve merge conflicts.

--- src/stack_dec/thot_server.cc
+++ src/stack_dec/thot_server.cc
@@ -198,10 +198,7 @@ int start_server(void)
   my_addr.sin_addr.s_addr = INADDR_ANY;  // fill with IP address
   memset(&(my_addr.sin_zero), '\0', 8);  // reset the rest of the data structure
 
-<<<<<<< HEAD
-=======
       // Assign address to socket
->>>>>>> concurrent-server
   if (::bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))== -1)
   {
     StdCerrThreadSafe<<"bind error"<<std::endl;
