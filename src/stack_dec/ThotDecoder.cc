7fc0dd5050157aa63d1b7f86be4fa87e96092e00
daormar <dortiz@prhlt.upv.es>
2017-11-23 17:59:10 +0100

    Modify information printed to error output.
    Use StrProcUtils::getLibDir function to print directory where
    so files are stored.

--- src/stack_dec/ThotDecoder.cc
+++ src/stack_dec/ThotDecoder.cc
@@ -35,7 +35,7 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 ThotDecoder::ThotDecoder()
 {
       // Print library directory path for so files
-  std::cerr<<StrProcUtils::getLibDirVarNameValue()<<" = "<<THOT_LIBDIR<<std::endl;
+  std::cerr<<StrProcUtils::getLibDirVarNameValue()<<" = "<<StrProcUtils::getLibDir()<<std::endl;
 
       // Determine which implementation is being used
   tdCommonVars.featureBasedImplEnabled=featureBasedImplIsEnabled();
