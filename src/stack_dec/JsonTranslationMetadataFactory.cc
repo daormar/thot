f8a736a036e199d475eae3a14c9012c9dd618a1f
Adam Harasimowicz <adam.ha@webinterpret.com>
2017-12-11 16:55:35 +0100

    Add building .so file for JsonTranslationMetadata class

---
+++ src/stack_dec/JsonTranslationMetadataFactory.cc
@@ -0,0 +1,43 @@
+/*
+thot package for statistical machine translation
+Copyright (C) 2017 Adam Harasimowicz
+ 
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+ 
+/********************************************************************/
+/*                                                                  */
+/* Module: JsonTranslationMetadataFactory                           */
+/*                                                                  */
+/* Definitions file: JsonTranslationMetadataFactory.cc              */
+/*                                                                  */
+/********************************************************************/
+
+
+//--------------- Include files --------------------------------------
+
+#include "JsonTranslationMetadata.h"
+
+//--------------- Function definitions
+
+extern "C" BaseTranslationMetadata* create(const char* /*str*/)
+{
+  return new JsonTranslationMetadata;
+}
+
+//---------------
+extern "C" const char* type_id(void)
+{
+  return "JsonTranslationMetadata";
+}
