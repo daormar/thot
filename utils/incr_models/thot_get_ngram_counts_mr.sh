c3ed58f9c25d7254bf3c778ce47e73a8dbcd15da
daormar <dortiz@prhlt.upv.es>
2017-12-15 15:48:47 +0100

    Add -f option.

--- utils/incr_models/thot_get_ngram_counts_mr.sh
+++ utils/incr_models/thot_get_ngram_counts_mr.sh
@@ -72,12 +72,14 @@ version()
 
 usage()
 {
-    echo "thot_get_ngram_counts_mr -c <string> -n <int> [-unk]"
+    echo "thot_get_ngram_counts_mr -c <string> -n <int> [-unk] [-f <int>]"
     echo "                          [-tdir <string>]"
     echo "                          [-debug] [--help] [--version]"
     echo ""
     echo "-c <string>        : Corpus file."
     echo "-n <int>           : Order of the n-grams."
+    echo "-f <int>           : Size in lines of the fragments in which the corpus"
+    echo "                     is divided when performing map-reduce (50K by default)."
     echo "-unk               : Reserve probability mass for the unknown word."
     echo "-tdir <string>     : Directory for temporary files."
     echo "-debug             : After ending, do not delete temporary files"
@@ -104,9 +106,11 @@ set_tmp_dir()
 unk_given=0
 c_given=0
 n_given=0
+f_given=0
+fragm_size=50000
 tdir="/tmp"
 debug=0
-chunk_size=50000
+fragm_size=50000
 
 if [ $# -eq 0 ]; then
     print_desc
@@ -135,6 +139,14 @@ while [ $# -ne 0 ]; do
                 n_given=1
             else
                 n_given=0
+            fi
+            ;;
+        "-f") shift
+            if [ $# -ne 0 ]; then
+                fragm_size=$1
+                f_given=1
+            else
+                f_given=0
             fi
             ;;
         "-tdir") shift
@@ -185,10 +197,10 @@ else
     proc_corpus=$corpus
 fi
 
-# Split corpus into chunks of fixed size
-${SPLIT} -a 4 -l ${chunk_size} $proc_corpus $TMP/
+# Split corpus into fragments of fixed size
+${SPLIT} -a 4 -l ${fragm_size} $proc_corpus $TMP/
 
-# Process chunks
+# Process fragments
 c=1
 for i in `ls $TMP`; do
     
