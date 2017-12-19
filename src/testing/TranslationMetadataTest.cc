6c385648cc61df69bb3a305755e4cb1955debf4c
daormar <dortiz@prhlt.upv.es>
2017-12-19 16:21:59 +0100

    Add new unit testing module.

---
+++ src/testing/TranslationMetadataTest.cc
@@ -0,0 +1,177 @@
+/*
+thot package for statistical machine translation
+Copyright (C) 2017 Daniel Ortiz-Mart\'inez
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/********************************************************************/
+/*                                                                  */
+/* Module: TranslationMetadataTest                                  */
+/*                                                                  */
+/* Definitions file: TranslationMetadataTest.cc                     */
+/*                                                                  */
+/********************************************************************/
+
+
+//--------------- Include files --------------------------------------
+
+#include "TranslationMetadataTest.h"
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( TranslationMetadataTest );
+
+//--------------- TranslationMetadataTest class functions
+//
+
+//---------------------------------------
+void TranslationMetadataTest::setUp()
+{
+    metadata = new TranslationMetadata();
+}
+
+//---------------------------------------
+void TranslationMetadataTest::tearDown()
+{
+    delete metadata;
+}
+
+//---------------------------------------
+void TranslationMetadataTest::testSafeRecallingObtainTransConstraints()
+{
+    metadata->clear();
+
+    // Call
+    metadata->obtainTransConstraints(xmlStr, 0);
+    std::vector<std::string> srcSentVec1 = metadata->getSrcSentVec();
+
+    // Call again without cleaning
+    metadata->obtainTransConstraints(xmlStr, 0);
+    std::vector<std::string> srcSentVec2 = metadata->getSrcSentVec();
+
+    CPPUNIT_ASSERT( srcSentVec1 == srcSentVec2);
+}
+
+//---------------------------------------
+void TranslationMetadataTest::testGetSrcSentVec()
+{
+    // Prepare expected answer
+    const std::string expectedSrcSent = "First and only T-shirt with logo 22.9cm 2x5";
+
+    metadata->clear();
+    metadata->obtainTransConstraints(xmlStr, 0);
+
+    std::vector<std::string> srcSentVec = metadata->getSrcSentVec();
+
+    CPPUNIT_ASSERT_EQUAL((int) StrProcUtils::stringToStringVector(expectedSrcSent).size(),  (int) srcSentVec.size());
+    CPPUNIT_ASSERT_EQUAL(expectedSrcSent, StrProcUtils::stringVectorToString(srcSentVec));
+}
+
+//---------------------------------------
+void TranslationMetadataTest::testGetTransForSrcPhr()
+{
+    metadata->obtainTransConstraints(xmlStr, 0);
+    std::vector<std::string> translation = metadata->getTransForSrcPhr(std::make_pair(1, 1));
+
+    std::vector<std::string> expectedTranslation;
+    expectedTranslation.push_back("premier");
+
+    CPPUNIT_ASSERT(expectedTranslation == translation);
+}
+
+//---------------------------------------
+void TranslationMetadataTest::testGetConstrainedSrcPhrases()
+{
+    // Prepare expected data structure
+    std::set<std::pair<PositionIndex,PositionIndex> > expectedConstraints;
+    expectedConstraints.insert(std::make_pair(1, 1));
+    expectedConstraints.insert(std::make_pair(4, 5));
+    expectedConstraints.insert(std::make_pair(6, 6));
+    expectedConstraints.insert(std::make_pair(7, 7));
+    expectedConstraints.insert(std::make_pair(8, 8));
+
+    // Obtain constraints for a given xml string
+    metadata->obtainTransConstraints(xmlStr, 0);
+    std::set<std::pair<PositionIndex,PositionIndex> > constraints = metadata->getConstrainedSrcPhrases();
+
+    CPPUNIT_ASSERT(expectedConstraints == constraints);
+}
+
+//---------------------------------------
+void TranslationMetadataTest::testSrcPhrAffectedByConstraint()
+{
+    bool isConstrained;
+    // Extract constraints
+    metadata->obtainTransConstraints(xmlStr, 0);
+
+    isConstrained = metadata->srcPhrAffectedByConstraint(std::make_pair(1, 1));
+    CPPUNIT_ASSERT_EQUAL(true, isConstrained);
+
+    isConstrained = metadata->srcPhrAffectedByConstraint(std::make_pair(1, 2));
+    CPPUNIT_ASSERT_EQUAL(true, isConstrained);
+
+    isConstrained = metadata->srcPhrAffectedByConstraint(std::make_pair(2, 2));
+    CPPUNIT_ASSERT_EQUAL(false, isConstrained);
+
+    isConstrained = metadata->srcPhrAffectedByConstraint(std::make_pair(5, 7));
+    CPPUNIT_ASSERT_EQUAL(true, isConstrained);
+
+    isConstrained = metadata->srcPhrAffectedByConstraint(std::make_pair(6, 8));
+    CPPUNIT_ASSERT_EQUAL(true, isConstrained);
+}
+
+//---------------------------------------
+void TranslationMetadataTest::testTranslationSatisfiesConstraints()
+{
+    // Extract constraints
+    metadata->obtainTransConstraints(xmlStr, 0);
+
+    // Prepare parameters
+    SourceSegmentation sourceSegmentation;
+    sourceSegmentation.push_back(std::make_pair(1, 1));
+    sourceSegmentation.push_back(std::make_pair(2, 3));
+    sourceSegmentation.push_back(std::make_pair(4, 5));
+    sourceSegmentation.push_back(std::make_pair(6, 6));
+    sourceSegmentation.push_back(std::make_pair(7, 7));
+    sourceSegmentation.push_back(std::make_pair(8, 8));
+
+    std::vector<PositionIndex> targetSegmentCuts;
+    targetSegmentCuts.push_back(1);
+    targetSegmentCuts.push_back(3);
+    targetSegmentCuts.push_back(5);
+    targetSegmentCuts.push_back(6);
+    targetSegmentCuts.push_back(7);
+    targetSegmentCuts.push_back(8);
+
+    std::vector<std::string> targetWordVec;
+    targetWordVec.push_back("premier");
+    targetWordVec.push_back("et");
+    targetWordVec.push_back("Only");
+    targetWordVec.push_back("t-shirt");
+    targetWordVec.push_back("avec");
+    targetWordVec.push_back("Logo");
+    targetWordVec.push_back("22.9cm");
+    targetWordVec.push_back("2x5");
+
+    // Valid translation - respects all constraints
+    bool isSatisfied = metadata->translationSatisfiesConstraints(sourceSegmentation, targetSegmentCuts, targetWordVec);
+    CPPUNIT_ASSERT_EQUAL(true, isSatisfied);
+
+    // Wrong cut which violates constraints
+    targetSegmentCuts[2] -= 1;
+
+    // Invalid translation - do not respect one constraint
+    isSatisfied = metadata->translationSatisfiesConstraints(sourceSegmentation, targetSegmentCuts, targetWordVec);
+    CPPUNIT_ASSERT_EQUAL(false, isSatisfied);
+}
