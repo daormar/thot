38da17359d1faefdc4db3a7aefd60d33c739a88b
daormar <dortiz@prhlt.upv.es>
2018-03-01 16:27:17 +0100

    Extend information returned by struct functions.
    struct functions now provide information about the indices that
    are related to each feature pointer.

--- src/stack_dec/FeaturesInfo.h
+++ src/stack_dec/FeaturesInfo.h
@@ -46,64 +46,84 @@ struct FeaturesInfo
   std::vector<BasePbTransModelFeature<SCORE_INFO>* > featPtrVec;
 
       // Functions to get pointers to features
-  std::vector<LangModelFeat<SCORE_INFO>* > getLangModelFeatPtrs(void);
-  std::vector<DirectPhraseModelFeat<SCORE_INFO>* > getDirectPhraseModelFeatPtrs(void);
-  std::vector<InversePhraseModelFeat<SCORE_INFO>* > getInversePhraseModelFeatPtrs(void);
-  std::vector<SrcPosJumpFeat<SCORE_INFO>* > getSrcPosJumpFeatPtrs(void);
+  std::vector<LangModelFeat<SCORE_INFO>* > getLangModelFeatPtrs(std::vector<unsigned int>& featIndexVec);
+  std::vector<DirectPhraseModelFeat<SCORE_INFO>* > getDirectPhraseModelFeatPtrs(std::vector<unsigned int>& featIndexVec);
+  std::vector<InversePhraseModelFeat<SCORE_INFO>* > getInversePhraseModelFeatPtrs(std::vector<unsigned int>& featIndexVec);
+  std::vector<SrcPosJumpFeat<SCORE_INFO>* > getSrcPosJumpFeatPtrs(std::vector<unsigned int>& featIndexVec);
 };
 
 //---------------
 template<class SCORE_INFO>
-std::vector<LangModelFeat<SCORE_INFO>* > FeaturesInfo<SCORE_INFO>::getLangModelFeatPtrs(void)
+std::vector<LangModelFeat<SCORE_INFO>* >
+FeaturesInfo<SCORE_INFO>::getLangModelFeatPtrs(std::vector<unsigned int>& featIndexVec)
 {
+  featIndexVec.clear();
   std::vector<LangModelFeat<SCORE_INFO>* > lmFeatPtrVec;
   for(unsigned int i=0;i<featPtrVec.size();++i)
   {
     LangModelFeat<SCORE_INFO>* lmFeatPtr=dynamic_cast<LangModelFeat<SCORE_INFO>* >(featPtrVec[i]);
     if(lmFeatPtr)
+    {
       lmFeatPtrVec.push_back(lmFeatPtr);
+      featIndexVec.push_back(i);
+    }
   }
   return lmFeatPtrVec;
 }
 
 //---------------
 template<class SCORE_INFO>
-std::vector<DirectPhraseModelFeat<SCORE_INFO>* > FeaturesInfo<SCORE_INFO>::getDirectPhraseModelFeatPtrs(void)
+std::vector<DirectPhraseModelFeat<SCORE_INFO>* >
+FeaturesInfo<SCORE_INFO>::getDirectPhraseModelFeatPtrs(std::vector<unsigned int>& featIndexVec)
 {
+  featIndexVec.clear();
   std::vector<DirectPhraseModelFeat<SCORE_INFO>* > directPhraseModelFeatPtrVec;
   for(unsigned int i=0;i<featPtrVec.size();++i)
   {
     DirectPhraseModelFeat<SCORE_INFO>* directPhraseModelFeatPtr=dynamic_cast<DirectPhraseModelFeat<SCORE_INFO>* >(featPtrVec[i]);
     if(directPhraseModelFeatPtr)
+    {
       directPhraseModelFeatPtrVec.push_back(directPhraseModelFeatPtr);
+      featIndexVec.push_back(i);
+    }
   }
   return directPhraseModelFeatPtrVec;
 }
 
 //---------------
 template<class SCORE_INFO>
-std::vector<InversePhraseModelFeat<SCORE_INFO>* > FeaturesInfo<SCORE_INFO>::getInversePhraseModelFeatPtrs(void)
+std::vector<InversePhraseModelFeat<SCORE_INFO>* >
+FeaturesInfo<SCORE_INFO>::getInversePhraseModelFeatPtrs(std::vector<unsigned int>& featIndexVec)
 {
+  featIndexVec.clear();
   std::vector<InversePhraseModelFeat<SCORE_INFO>* > inversePhraseModelFeatPtrVec;
   for(unsigned int i=0;i<featPtrVec.size();++i)
   {
     InversePhraseModelFeat<SCORE_INFO>* inversePhraseModelFeatPtr=dynamic_cast<InversePhraseModelFeat<SCORE_INFO>* >(featPtrVec[i]);
     if(inversePhraseModelFeatPtr)
+    {
       inversePhraseModelFeatPtrVec.push_back(inversePhraseModelFeatPtr);
+      featIndexVec.push_back(i);
+    }
   }
   return inversePhraseModelFeatPtrVec;
 }
 
 //---------------
 template<class SCORE_INFO>
-std::vector<SrcPosJumpFeat<SCORE_INFO>* > FeaturesInfo<SCORE_INFO>::getSrcPosJumpFeatPtrs(void)
+std::vector<SrcPosJumpFeat<SCORE_INFO>* >
+FeaturesInfo<SCORE_INFO>::getSrcPosJumpFeatPtrs(std::vector<unsigned int>& featIndexVec)
 {
+  featIndexVec.clear();
   std::vector<SrcPosJumpFeat<SCORE_INFO>* > srcPosJumpFeatPtrVec;
   for(unsigned int i=0;i<featPtrVec.size();++i)
   {
     SrcPosJumpFeat<SCORE_INFO>* srcPosJumpFeatPtr=dynamic_cast<SrcPosJumpFeat<SCORE_INFO>* >(featPtrVec[i]);
     if(srcPosJumpFeatPtr)
+    {
       srcPosJumpFeatPtrVec.push_back(srcPosJumpFeatPtr);
+      featIndexVec.push_back(i);
+    }
   }
   return srcPosJumpFeatPtrVec;  
 }
