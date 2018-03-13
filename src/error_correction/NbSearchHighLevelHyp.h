71f744ccf558e17de83f3c3c3a1e616f87c10fea
daormar <dortiz@prhlt.upv.es>
2018-03-13 22:14:22 +0100

    Add new file.

---
+++ src/error_correction/NbSearchHighLevelHyp.h
@@ -0,0 +1,35 @@
+/*
+thot package for statistical machine translation
+Copyright (C) 2013 Daniel Ortiz-Mart\'inez
+ 
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+ 
+#ifndef _NbSearchHighLevelHyp_h
+#define _NbSearchHighLevelHyp_h
+
+//--------------- Include files --------------------------------------
+
+#if HAVE_CONFIG_H
+#  include <thot_config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include "WordGraphArc.h"
+#include <vector>
+
+//--------------- typedefs -------------------------------------------
+
+typedef std::vector<WordGraphArc> NbSearchHighLevelHyp;
+
+#endif
