c10b7e3a55bca33316ff844a34b9eb9f03150172
daormar <dortiz@prhlt.upv.es>
2018-03-16 17:38:50 +0100

    Add new factory.

---
+++ src/stack_dec/DictFeatPhrScoreInfoFactory.cc
@@ -0,0 +1,41 @@
+/*
+thot package for statistical machine translation
+Copyright (C) 2013 Daniel Ortiz-Mart\'inez
+ 
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file DictFeatPhrScoreInfoFactory.cc
+ * 
+ * @brief Factory for DictFeat objects.
+ */
+
+//--------------- Include files --------------------------------------
+
+#include "PhrScoreInfo.h"
+#include "DictFeat.h"
+
+//--------------- Function definitions
+
+extern "C" BasePbTransModel<PhrScoreInfo>* create(const char* /*str*/)
+{
+  return new DictFeat;
+}
+
+//---------------
+extern "C" const char* type_id(void)
+{
+  return "DictFeat";
+}
