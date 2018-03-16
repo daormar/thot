b3f45bb70948525732f87a1b75657f89289beccc
daormar <dortiz@prhlt.upv.es>
2018-03-16 17:24:01 +0100

    Remove compiler warning.

--- src/stack_dec/LangModelFeat.cc
+++ src/stack_dec/LangModelFeat.cc
@@ -31,7 +31,7 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 template<>
 LangModelFeat<PhrScoreInfo>::HypScoreInfo
 LangModelFeat<PhrScoreInfo>::nullHypScore(const HypScoreInfo& predHypScrInf,
-                                          float weight,
+                                          float /*weight*/,
                                           Score& unweightedScore)
 {
   unweightedScore=0;
