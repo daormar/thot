fad39fc5f6d1b6c4ca66acd642382a1d50faa122
daormar <dortiz@prhlt.upv.es>
2018-03-27 17:15:37 +0200

    Add missing include directives.

--- src/stack_dec/DictFeat.h
+++ src/stack_dec/DictFeat.h
@@ -37,6 +37,7 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 #include "AwkInputStream.h"
 #include "Score.h"
 #include "ErrorDefs.h"
+#include <stdexcept>
 #include <map>
 
 //--------------- Constants ------------------------------------------
--- src/stack_dec/LevelDbDictFeat.h
+++ src/stack_dec/LevelDbDictFeat.h
@@ -37,6 +37,7 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 #include "BasePbTransModelFeature.h"
 #include "Score.h"
 #include "ErrorDefs.h"
+#include <stdexcept>
 #include <map>
 
 //--------------- Constants ------------------------------------------
